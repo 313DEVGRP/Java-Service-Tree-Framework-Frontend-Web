5cd07d4d43536675e4294d68d4c75c61207ab65a
LeeDongMin <admin@313.co.kr>
2023-02-13 22:51:04 +0900

    [ARMSQA-19] #comment select2 한글 버그로인한 버전 교체 - 23.02.13 #close #time 1h +review SR @dongmin.lee
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- arms/js/reqReview.js
+++ arms/js/reqReview.js
@@ -15,9 +15,30 @@ function execDocReady() {
 
 	getJsonForPrototype("./mock/reviewClassify.json", makeClassifyMenus);
 
-	var externalData = "";
-	var jquerySelectorID = "#reqReviewTable";
-	var ajaxUrl = "/auth-user/api/arms/reqReview/getMonitor_Without_Root.do?reviewer=" + userName + "&filter=All";
+	dataTableLoad();
+}
+
+// make review classify menu
+var makeClassifyMenus = function (data) {
+	var reviewClassify = document.getElementById("review-classify");
+	var menus = "";
+	data.forEach(
+		(item) =>
+			(menus += `
+		<li class="${item.current ? "active" : ""}" data-c_review_result_state="${item.c_review_result_state}">
+			<a href="#">${item.name}</a>
+		</li>
+	`)
+	);
+	reviewClassify.innerHTML = menus;
+};
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+// --- 데이터 테이블 설정 --- //
+////////////////////////////////////////////////////////////////////////////////////////
+function dataTableLoad() {
+	// 데이터 테이블 컬럼 및 열그룹 구성
 	var columnList = [
 		{ name: "c_id",
 			title: "ID",
@@ -64,138 +85,33 @@ function execDocReady() {
 	var rowsGroupList = [];
 	var columnDefList = [];
 	var selectList = {};
-	var buttonList = [];
-	reqStatusDataTable = common_dataTableLoad(externalData, jquerySelectorID, ajaxUrl, columnList, rowsGroupList, columnDefList, selectList, buttonList);
-
-}
-
-// make review classify menu
-var makeClassifyMenus = function (data) {
-	var reviewClassify = document.getElementById("review-classify");
-	var menus = "";
-	data.forEach(
-		(item) =>
-			(menus += `
-		<li class="${item.current ? "active" : ""}" data-c_review_result_state="${item.c_review_result_state}">
-			<a href="#">${item.name}</a>
-		</li>
-	`)
-	);
-	reviewClassify.innerHTML = menus;
-};
-
-////////////////////////////////////////////////////////////////////////////////////////
-//데이터 테이블
-////////////////////////////////////////////////////////////////////////////////////////
-// -------------------- 데이터 테이블을 만드는 템플릿으로 쓰기에 적당하게 리팩토링 함. ------------------ //
-function common_dataTableLoad(externalData ,jquerySelectorID, ajaxUrl, columnList, rowsGroupList, columnDefList, selectList, buttonList) {
-
-	var jQueryElementID = jquerySelectorID;
-	var reg = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi;
-	var jQueryElementStr = jQueryElementID.replace(reg,'');
-	// 데이터 테이블 컬럼 및 열그룹 구성
 	var orderList = [[ 1, 'asc' ]];
-	console.log("defaultType_dataTableLoad externalData -> " + externalData);
-	console.log("dataTableBuild :: jQueryElementID -> " + jQueryElementID);
-	console.log("dataTableBuild :: jQueryElementStr -> " + jQueryElementStr);
-	console.log("dataTableBuild :: columnList -> " + columnList);
-	console.log("dataTableBuild :: rowsGroupList -> " + rowsGroupList);
-	console.log("dataTableBuild :: href: " + $(location).attr("href"));
-	console.log("dataTableBuild :: protocol: " + $(location).attr("protocol"));
-	console.log("dataTableBuild :: host: " + $(location).attr("host"));
-	console.log("dataTableBuild :: pathname: " + $(location).attr("pathname"));
-	console.log("dataTableBuild :: search: " + $(location).attr("search"));
-	console.log("dataTableBuild :: hostname: " + $(location).attr("hostname"));
-	console.log("dataTableBuild :: port: " + $(location).attr("port"));
+	var buttonList = [];
 
-	var tempDataTable = $(jQueryElementID).DataTable({
-		ajax: {
-			url: ajaxUrl,
-			dataSrc: 'result'
-		},
-		serverSide: true,
-		destroy: true,
-		processing: true,
-		responsive: false,
-		columns: columnList,
-		rowsGroup: rowsGroupList,
-		columnDefs: columnDefList,
-		select: selectList,
-		order: orderList,
-		buttons: buttonList,
-		language: {
-			processing: '<img src="./img/earth.gif" height="90px" width="90px"> '
-		},
-		initComplete: function(settings, json) {
-			console.log("dataTableBuild :: drawCallback");
-			if ($.isFunction(dataTableCallBack )) {
-				dataTableCallBack(settings, json);
-			}
-		},
-		fnInitComplete: function(oSettings, json) {
-					if(json.message == undefined){
-						console.log("정상");
-					}else{
-						console.log("비정상");
-					}
-		}
-	});
+	var jquerySelector = "#reqReviewTable";
+	var ajaxUrl = "/auth-user/api/arms/reqReview/getMonitor_Without_Root.do?reviewer=" + userName + "&filter=All";
+	var jsonRoot = "result";
 
-	$(jQueryElementID + " tbody").on("click", "tr", function () {
-
-		if ($(this).hasClass("selected")) {
-			$(this).removeClass("selected");
-		} else {
-			tempDataTable.$("tr.selected").removeClass("selected");
-			$(this).addClass("selected");
-		}
-
-		var selectedData = tempDataTable.row(this).data();
-		selectedData.selectedIndex = $(this).closest('tr').index();
-
-		var info = tempDataTable.page.info();
-		console.log( 'Showing page: '+info.page+' of '+info.pages );
-		selectedData.selectedPage = info.page;
-
-		dataTableClick(selectedData);
-	});
-
-	// ----- 데이터 테이블 빌드 이후 스타일 구성 ------ //
-	//datatable 좌상단 datarow combobox style
-	$(".dataTables_length").find("select:eq(0)").addClass("darkBack");
-	$(".dataTables_length").find("select:eq(0)").css("min-height", "30px");
-	//min-height: 30px;
-
-	// ----- 데이터 테이블 빌드 이후 별도 스타일 구성 ------ //
-	//datatable 좌상단 datarow combobox style
-	$("body").find("[aria-controls='" + jQueryElementStr + "']").css("width", "100px");
-	$("select[name=" + jQueryElementStr + "]").css("width", "50px");
-
-	$("select[name=" + jQueryElementStr + "_length] option").css("background", "#41434A");
-	$("select[name=" + jQueryElementStr + "_length]").css("border", "1px solid blue");
-
-	$.fn.dataTable.ext.errMode = function ( settings, helpPage, message ) {
-		console.log(message);
-		jError("Notification : <strong>Ajax Error</strong>, Complete !");
-	};
-
-	return tempDataTable;
+	dataTableRef = dataTable_build(jquerySelector, ajaxUrl, jsonRoot, columnList, rowsGroupList, columnDefList, selectList, orderList, buttonList);
 }
-
 
 // -------------------- 데이터 테이블을 만드는 템플릿으로 쓰기에 적당하게 리팩토링 함. ------------------ //
 
 // 데이터 테이블 구성 이후 꼭 구현해야 할 메소드 : 열 클릭시 이벤트
-function dataTableClick(selectedData) {
+function dataTableClick(tempDataTable, selectedData) {
 	console.log("selectedData.c_review_pdservice_link = " + selectedData.c_review_pdservice_link);
 	console.log("selectedData.c_review_req_link = " + selectedData.c_review_req_link);
 	console.log("selectedData.c_id = " + selectedData.c_id);
-	location.href = "reqReviewDetail.html?c_id=" + selectedData.c_id + "&c_review_pdservice_link=" + selectedData.c_review_pdservice_link + "&c_review_req_link=" + selectedData.c_review_req_link;
+	location.href = "template.html?page=reqReviewDetail&c_id=" + selectedData.c_id + "&c_review_pdservice_link=" + selectedData.c_review_pdservice_link + "&c_review_req_link=" + selectedData.c_review_req_link;
 }
 
-// 데이터 테이블 데이터 렌더링 이후 콜백 함수.
+//데이터 테이블 ajax load 이후 콜백.
 function dataTableCallBack(settings, json){
 
+}
+
+function dataTableDrawCallback(tableInfo) {
+	$("#" + tableInfo.sInstance).DataTable().columns.adjust().responsive.recalc();
 }
 
 
