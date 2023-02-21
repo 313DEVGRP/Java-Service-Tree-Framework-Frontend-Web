723310255b29d1166c19f77fca638a86399e7390
LeeDongMin <admin@313.co.kr>
2023-02-21 20:01:43 +0900

    [ARMSQA-19] #comment 데이터테이블 렌더링 추가 및 intro 추가 - 23.02.21 #close #time 1h +review SR @dongmin.lee
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- arms/js/common.js
+++ arms/js/common.js
@@ -696,6 +696,27 @@ var isEmpty = function (value) {
 ////////////////////////////////////////////////////////////////////////////////////////
 //데이터 테이블
 ////////////////////////////////////////////////////////////////////////////////////////
+// var responsiveRender = {
+// 	details: {
+// 		renderer: function(api, rowIdx, columns) {
+// 			var outer = "<tr data-dt-row=" + rowIdx + " data-dt-column='0'><td>";
+// 			var data = $.map(columns, function(col, i) {
+// 				return col.hidden ?
+// 					"<div class='gradient_bottom_border' style='margin-bottom: 5px;float:left;width:180px;'>" +
+// 					"<div style='text-align: center;padding:3px;background: #3b3d40'><strong>" + col.title + "</strong></div>" +
+// 					"<div style='padding:3px;text-align: center;color: #a4c6ff;'>" + col.data + "</div>" +
+// 					"</div>" : "";
+// 			}).join("");
+// 			outer += data;
+// 			outer += "</td></tr>";
+// 			data = outer;
+//
+// 			return data ?
+// 				$("<table/>").append(data) :
+// 				false;
+// 		}
+// 	}
+// };
 function dataTable_build(
 	jquerySelector,
 	ajaxUrl,
@@ -705,12 +726,36 @@ function dataTable_build(
 	columnDefList,
 	selectList,
 	orderList,
-	buttonList,
-	isServerSide
+	buttonList
 ) {
+	var responsiveRender = true;
+	return dataTable_childRenderBuild(jquerySelector,
+		ajaxUrl,
+		jsonRoot,
+		responsiveRender,
+		columnList,
+		rowsGroupList,
+		columnDefList,
+		selectList,
+		orderList,
+		buttonList);
+}
+
+function dataTable_childRenderBuild(jquerySelector,
+																															ajaxUrl,
+																															jsonRoot,
+																															responsiveRender,
+																															columnList,
+																															rowsGroupList,
+																															columnDefList,
+																															selectList,
+																															orderList,
+																															buttonList) {
+
 	var jQueryElementID = jquerySelector;
 	var reg = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi;
 	var jQueryElementStr = jQueryElementID.replace(reg, "");
+
 	console.log("dataTableBuild :: jQueryElementStr → " + jQueryElementStr);
 	console.log("dataTableBuild :: jQueryElementID → " + jQueryElementID);
 	console.log("dataTableBuild :: columnList → " + columnList);
@@ -871,3 +916,18 @@ function ajax_sample() {
 		}
 	});
 }
+
+
+//데이터 테이블 하위에 상세 리스트 보이는거 지우기
+// Enumerate all rows
+$("#hostTable")
+	.DataTable()
+	.rows()
+	.every(function() {
+		// If row has details expanded
+		if (this.child.isShown()) {
+			// Collapse row details
+			this.child.hide();
+			$(this.node()).removeClass("shown");
+		}
+	});
