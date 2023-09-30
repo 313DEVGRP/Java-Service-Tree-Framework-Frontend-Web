0de547525da81116ebb519cf11a88283c3052e69
Advanc2d <phm940916@gmail.com>
2023-09-30 12:06:10 +0900

    :see_no_evil: refacor : [Frontend] #comment js로드 오류 부분 수정 - 23.09.30
    #close #time 1h +review SR @313devops
    
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::Advanc2d ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- arms/js/detail.js
+++ arms/js/detail.js
@@ -14,11 +14,11 @@ function execDocReady() {
     var pluginGroups = [
         [
             // Vendor JS Files
-            /*"../reference/jquery-plugins/MyResume/assets/vendor/purecounter/purecounter_vanilla.js",
+            "../reference/jquery-plugins/MyResume/assets/vendor/purecounter/purecounter_vanilla.js",
             "../reference/jquery-plugins/MyResume/assets/vendor/glightbox/js/glightbox.min.js",
             "../reference/jquery-plugins/MyResume/assets/vendor/swiper/swiper-bundle.min.js",
             // Template Main JS File
-            "../reference/jquery-plugins/MyResume/assets/js/main.js"*/
+            "../reference/jquery-plugins/MyResume/assets/js/main.js"
         ],
 
         [
@@ -43,11 +43,11 @@ function execDocReady() {
 
         [
             // Template CSS File
-            /*            "../reference/jquery-plugins/MyResume/assets/vendor/boxicons/css/boxicons.css",
-                        "../reference/jquery-plugins/MyResume/assets/vendor/glightbox/css/glightbox.min.css",
-                        "../reference/jquery-plugins/MyResume/assets/vendor/swiper/swiper-bundle.min.css",
-                        // Template Main CSS File
-                        "../reference/jquery-plugins/MyResume/assets/css/style.css"*/
+            "../reference/jquery-plugins/MyResume/assets/vendor/boxicons/css/boxicons.css",
+            "../reference/jquery-plugins/MyResume/assets/vendor/glightbox/css/glightbox.min.css",
+            "../reference/jquery-plugins/MyResume/assets/vendor/swiper/swiper-bundle.min.css",
+            // Template Main CSS File
+            "../reference/jquery-plugins/MyResume/assets/css/style.css"
         ]
         // 추가적인 플러그인 그룹들을 이곳에 추가하면 됩니다.
     ];
@@ -150,7 +150,7 @@ function checkVisible( element, check = 'above' ) {
         return ((y < (viewportHeight + scrolltop)));
 }
 
-const scrollApiFunc = function () {
+var scrollApiFunc = function () {
     for (let element in visibilityStatus) {
         if (!visibilityStatus[element] && checkVisible(element)) {
 
