64aefe71455ea2c690f4fac121919ba0fe1d963b
jiyoondev <jiyoon.jang.dev@gmail.com>
2023-10-08 11:18:20 +0900

    feat: [ARMSQA-23] #comment 대시보드의 서비스 선택 위젯 구현 23.10.08
    #close #time 10m +review SR @jiyoondev
    
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    요구 관리 : [Requirement Manage] http://www.a-rms.net
    문서 관리 : [Document] http://www.313.co.kr/confluence
    이슈 관리 : [IssueTracker] http://www.313.co.kr/jira
    소스 관리 : [VersionControl] https://github.com/313devgrp
    코드 리뷰 : [CodeReview] http://www.313.co.kr/fecru
    빌드 관리 : [CICD-Deploy] http://www.313.co.kr/jenkins
    배포 관리 : [Deploy-Pipeline] http://www.313.co.kr/spinnaker
    아티 팩트 : [ArtifactManager] http://www.313.co.kr/nexus
    코드 분석 : [CodeAnalysis] http://www.313.co.kr/sonar
    성능 측정 : [ZipKin] http://www.313.co.kr/zipkin/
    
    도커 관리 : [Docker Hub] https://hub.docker.com/u/313devgrp
    클러스터1 : [Kubernetes] http://www.313.co.kr:31380
    클러스터2 : [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    디비 관리 : [DB Admin] http://www.313.co.kr/phpmyadmin/
    오브 젝트 : [S3 Admin] http://www.313.co.kr/minio/login
    캐시 관리 : [Cache Admin] http://www.313.co.kr/redis/
    ELFK 스택1 : [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    ELFK 스택2 : [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    ELFK 스택3 : [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    ELFK 스택4 : [Kibana] http://www.313.co.kr/kibana/app/home#/
    
    모니터링 1 : [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    모니터링 2 : [Grafana] http://www.313.co.kr/grafana
    
    인증 인가 : [Auth] http://www.313.co.kr/auth/
    
    NFS 관리 : [NAS] http://www.313.co.kr:5000/webman/index.cgi
    메일 관리 : [Mail] http://www.313.co.kr/mail/
    원격 관리 : [RDP] http://www.313.co.kr/guacamole/#/

--- arms/js/dashboard.js
+++ arms/js/dashboard.js
@@ -1,54 +1,210 @@
 ////////////////////////////////////////////////////////////////////////////////////////
 //Document Ready
 ////////////////////////////////////////////////////////////////////////////////////////
+var selectedPdServiceId; // 제품(서비스) 아이디
+
 function execDocReady() {
 
-    var pluginGroups = [
-        [	"../reference/light-blue/lib/vendor/jquery.ui.widget.js",
-            "../reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Templates_js_tmpl.js",
-            "../reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Load-Image_js_load-image.js",
-            "../reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Canvas-to-Blob_js_canvas-to-blob.js",
-            "../reference/light-blue/lib/jquery.iframe-transport.js",
-            "../reference/light-blue/lib/jquery.fileupload.js",
-            "../reference/light-blue/lib/jquery.fileupload-fp.js",
-            "../reference/light-blue/lib/jquery.fileupload-ui.js"],
+	var pluginGroups = [
+		["../reference/light-blue/lib/vendor/jquery.ui.widget.js",
+			"../reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Templates_js_tmpl.js",
+			"../reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Load-Image_js_load-image.js",
+			"../reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Canvas-to-Blob_js_canvas-to-blob.js",
+			"../reference/light-blue/lib/jquery.iframe-transport.js",
+			"../reference/light-blue/lib/jquery.fileupload.js",
+			"../reference/light-blue/lib/jquery.fileupload-fp.js",
+			"../reference/light-blue/lib/jquery.fileupload-ui.js"],
 
-        [	"../reference/jquery-plugins/select2-4.0.2/dist/css/select2_lightblue4.css",
-            "../reference/jquery-plugins/lou-multi-select-0.9.12/css/multiselect-lightblue4.css",
-            "../reference/jquery-plugins/multiple-select-1.5.2/dist/multiple-select-bluelight.css",
-            "../reference/jquery-plugins/select2-4.0.2/dist/js/select2.min.js",
-            "../reference/jquery-plugins/lou-multi-select-0.9.12/js/jquery.quicksearch.js",
-            "../reference/jquery-plugins/lou-multi-select-0.9.12/js/jquery.multi-select.js",
-            "../reference/jquery-plugins/multiple-select-1.5.2/dist/multiple-select.min.js"],
+		["../reference/jquery-plugins/select2-4.0.2/dist/css/select2_lightblue4.css",
+			"../reference/jquery-plugins/lou-multi-select-0.9.12/css/multiselect-lightblue4.css",
+			"../reference/jquery-plugins/multiple-select-1.5.2/dist/multiple-select-bluelight.css",
+			"../reference/jquery-plugins/select2-4.0.2/dist/js/select2.min.js",
+			"../reference/jquery-plugins/lou-multi-select-0.9.12/js/jquery.quicksearch.js",
+			"../reference/jquery-plugins/lou-multi-select-0.9.12/js/jquery.multi-select.js",
+			"../reference/jquery-plugins/multiple-select-1.5.2/dist/multiple-select.min.js"],
 
-        [	"../reference/jquery-plugins/datetimepicker-2.5.20/build/jquery.datetimepicker.min.css",
-            "../reference/light-blue/lib/bootstrap-datepicker.js",
-            "../reference/jquery-plugins/datetimepicker-2.5.20/build/jquery.datetimepicker.full.min.js",
-            "../reference/lightblue4/docs/lib/widgster/widgster.js"],
+		["../reference/jquery-plugins/datetimepicker-2.5.20/build/jquery.datetimepicker.min.css",
+			"../reference/light-blue/lib/bootstrap-datepicker.js",
+			"../reference/jquery-plugins/datetimepicker-2.5.20/build/jquery.datetimepicker.full.min.js",
+			"../reference/lightblue4/docs/lib/widgster/widgster.js",
+			"../reference/lightblue4/docs/lib/slimScroll/jquery.slimscroll.min.js"],
 
-        [	"../reference/jquery-plugins/dataTables-1.10.16/media/css/jquery.dataTables_lightblue4.css",
-            "../reference/jquery-plugins/dataTables-1.10.16/extensions/Responsive/css/responsive.dataTables_lightblue4.css",
-            "../reference/jquery-plugins/dataTables-1.10.16/extensions/Select/css/select.dataTables_lightblue4.css",
-            "../reference/jquery-plugins/dataTables-1.10.16/media/js/jquery.dataTables.min.js",
-            "../reference/jquery-plugins/dataTables-1.10.16/extensions/Responsive/js/dataTables.responsive.min.js",
-            "../reference/jquery-plugins/dataTables-1.10.16/extensions/Select/js/dataTables.select.min.js",
-            "../reference/jquery-plugins/dataTables-1.10.16/extensions/RowGroup/js/dataTables.rowsGroup.min.js",
-            "../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/dataTables.buttons.min.js",
-            "../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/buttons.html5.js",
-            "../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/buttons.print.js",
-            "../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/jszip.min.js",
-            "../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/pdfmake.min.js"]
-        // 추가적인 플러그인 그룹들을 이곳에 추가하면 됩니다.
-    ];
+		["../reference/jquery-plugins/dataTables-1.10.16/media/css/jquery.dataTables_lightblue4.css",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Responsive/css/responsive.dataTables_lightblue4.css",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Select/css/select.dataTables_lightblue4.css",
+			"../reference/jquery-plugins/dataTables-1.10.16/media/js/jquery.dataTables.min.js",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Responsive/js/dataTables.responsive.min.js",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Select/js/dataTables.select.min.js",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/RowGroup/js/dataTables.rowsGroup.min.js",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/dataTables.buttons.min.js",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/buttons.html5.js",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/buttons.print.js",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/jszip.min.js",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/pdfmake.min.js"]
+		// 추가적인 플러그인 그룹들을 이곳에 추가하면 됩니다.
+	];
 
-    loadPluginGroupsParallelAndSequential(pluginGroups)
-        .then(function() {
-            // 사이드 메뉴 색상 설정
-            $('.widget').widgster();
-            setSideMenu("sidebar_menu_dashboard", "sidebar_menu_dashboard_home");
-        })
-        .catch(function() {
-            console.error('플러그인 로드 중 오류 발생');
-        });
+	loadPluginGroupsParallelAndSequential(pluginGroups)
+		.then(function () {
 
+			//vfs_fonts 파일이 커서 defer 처리 함.
+			setTimeout(function () {
+				var script = document.createElement("script");
+				script.src = "../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/vfs_fonts.js";
+				script.defer = true; // defer 속성 설정
+				document.head.appendChild(script);
+			}, 3000); // 2초 후에 실행됩니다.
+			console.log('모든 플러그인 로드 완료');
+
+			// 사이드 메뉴 색상 설정
+			$('.widget').widgster();
+			setSideMenu("sidebar_menu_dashboard_home");
+
+			makePdServiceSelectBox();
+		})
+		.catch(function () {
+			console.error('플러그인 로드 중 오류 발생');
+		});
+}
+////////////////////////////////////////////////////////////////////////////////////////
+//제품 서비스 셀렉트 박스
+////////////////////////////////////////////////////////////////////////////////////////
+function makePdServiceSelectBox() {
+	//제품 서비스 셀렉트 박스 이니시에이터
+	$(".chzn-select").each(function () {
+		$(this).select2($(this).data());
+	});
+
+	//제품 서비스 셀렉트 박스 데이터 바인딩
+	$.ajax({
+		url: "/auth-user/api/arms/pdService/getPdServiceMonitor.do",
+		type: "GET",
+		contentType: "application/json;charset=UTF-8",
+		dataType: "json",
+		progress: true,
+		statusCode: {
+			200: function (data) {
+				//////////////////////////////////////////////////////////
+				for (var k in data.response) {
+					var obj = data.response[k];
+					var newOption = new Option(obj.c_title, obj.c_id, false, false);
+					$("#selected_pdService").append(newOption).trigger("change");
+				}
+				//////////////////////////////////////////////////////////
+			}
+		}
+	});
+
+
+	$("#selected_pdService").on("select2:open", function () {
+		//슬림스크롤
+		makeSlimScroll(".select2-results__options");
+	});
+
+	// --- select2 ( 제품(서비스) 검색 및 선택 ) 이벤트 --- //
+	$("#selected_pdService").on("select2:select", function (e) {
+		// 제품( 서비스 ) 선택했으니까 자동으로 버전을 선택할 수 있게 유도
+		// 디폴트는 base version 을 선택하게 하고 ( select all )
+		//~> 이벤트 연계 함수 :: Version 표시 jsTree 빌드
+		bind_VersionData_By_PdService();
+
+		var checked = $("#checkbox1").is(":checked");
+		var endPointUrl = "";
+
+		if (checked) {
+			endPointUrl = "/T_ARMS_REQSTATUS_" + $("#selected_pdService").val() + "/getStatusMonitor.do?disable=true";
+		} else {
+			endPointUrl = "/T_ARMS_REQSTATUS_" + $("#selected_pdService").val() + "/getStatusMonitor.do?disable=false";
+		}
+
+		// progressLoad($("#selected_pdService").val(), null);
+	});
+} // end makePdServiceSelectBox()
+
+function progressLoad(pdservice_id, pdservice_version_id){
+
+	$('#progress_status').empty(); // 모든 자식 요소 삭제
+
+	//제품 서비스 셀렉트 박스 데이터 바인딩
+	$.ajax({
+		url: "/auth-user/api/arms/reqStatus/T_ARMS_REQSTATUS_" + pdservice_id + "/getProgress.do?version=" + pdservice_version_id,
+		type: "GET",
+		contentType: "application/json;charset=UTF-8",
+		dataType: "json",
+		progress: true,
+		statusCode: {
+			200: function (data) {
+
+				for (var key in data) {
+					var value = data[key];
+					console.log(key + "=" + value);
+
+					var html_piece = 	"<div	class=\"controls form-group darkBack\"\n" +
+						"		style=\"margin-bottom: 5px !important; padding-top: 5px !important;\">\n" +
+						"<span>✡ " + key + " : <a id=\"alm_server_count\" style=\"font-weight: bold;\"> " + value + "</a> 개</span>\n" +
+						"</div>";
+					$('#progress_status').append(html_piece);
+				}
+
+			}
+		}
+	});
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//버전 멀티 셀렉트 박스
+////////////////////////////////////////////////////////////////////////////////////////
+function makeVersionMultiSelectBox() {
+	//버전 선택 셀렉트 박스 이니시에이터
+	$(".multiple-select").multipleSelect({
+		filter: true,
+		onClose: function () {
+			console.log("onOpen event fire!\n");
+
+			var checked = $("#checkbox1").is(":checked");
+			var endPointUrl = "";
+			var versionTag = $(".multiple-select").val();
+
+			if (checked) {
+				endPointUrl =
+					"/T_ARMS_REQSTATUS_" + $("#selected_pdService").val() + "/getStatusMonitor.do?disable=true&versionTag=" + versionTag;
+				// dataTableLoad($("#selected_pdService").val(), endPointUrl);
+			} else {
+				endPointUrl =
+					"/T_ARMS_REQSTATUS_" + $("#selected_pdService").val() + "/getStatusMonitor.do?disable=false&versionTag=" + versionTag;
+				// dataTableLoad($("#selected_pdService").val(), endPointUrl);
+			}
+		}
+	});
+}
+
+function bind_VersionData_By_PdService() {
+	$(".multiple-select option").remove();
+	$.ajax({
+		url: "/auth-user/api/arms/pdService/getVersionList.do?c_id=" + $("#selected_pdService").val(),
+		type: "GET",
+		dataType: "json",
+		progress: true,
+		statusCode: {
+			200: function (data) {
+				//////////////////////////////////////////////////////////
+				for (var k in data.response) {
+					var obj = data.response[k];
+					var $opt = $("<option />", {
+						value: obj.c_id,
+						text: obj.c_title
+					});
+					$(".multiple-select").append($opt);
+				}
+
+				if (data.length > 0) {
+					console.log("display 재설정.");
+				}
+				//$('#multiversion').multipleSelect('refresh');
+				//$('#edit_multi_version').multipleSelect('refresh');
+				$(".multiple-select").multipleSelect("refresh");
+				//////////////////////////////////////////////////////////
+			}
+		}
+	});
 }
