5b56d00b10fb6fc6317d746e1b5d8b8d1b74f581
ssoohyun <chltngus129@gmail.com>
2023-10-09 22:06:50 +0900

    :boom: refactor : [Frontend] #comment 통계 정보 및 버전 상세 페이지 수정 - 23.10.09 1. 타이틀 페이지와 통계 정보 페이지 분리 및 페이지 아이콘 추가 2. 통계 정보 페이지 css 수정 및 요구사항 없을 경우 처리 3. 버전 상세 페이지 css 수정 4. 헤더에 뜨는 사용자 이름 간략하게 정리
    #close #time 1h +review SR @313devops
    
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    요구 관리 : [Requirement Manage] http://www.a-rms.net
    문서 관리 : [Document] http://www.313.co.kr/confluence
    이슈 관리 : [IssueTracker] http://www.313.co.kr/jira
    소스 관리 : [VersionControl] https://github.com/313devgrp
    코드 리뷰 : [CodeReview] http://www.313.co.kr/fecru
    빌드 관리 : [CICD-Deploy] http://www.313.co.kr/jenkins
    배포 관리 : [Deploy-Pipeline] http://www.313.co.kr/spinnaker
    아티 팩트 : [ArtifactManager] http://www.313.co.kr/nexus
    코드 분석 : [CodeAnalysis] http://www.313.co.kr/sonar
    성능 측정 : [ZipKin] http://www.313.co.kr/zipkin/
    
    도커 관리 : [Docker Hub] https://hub.docker.com/u/313devgrp
    클러스터1 : [Kubernetes] http://www.313.co.kr:31380
    클러스터2 : [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    디비 관리 : [DB Admin] http://www.313.co.kr/phpmyadmin/
    오브 젝트 : [S3 Admin] http://www.313.co.kr/minio/login
    캐시 관리 : [Cache Admin] http://www.313.co.kr/redis/
    ELFK 스택1 : [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    ELFK 스택2 : [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    ELFK 스택3 : [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    ELFK 스택4 : [Kibana] http://www.313.co.kr/kibana/app/home#/
    
    모니터링 1 : [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    모니터링 2 : [Grafana] http://www.313.co.kr/grafana
    
    인증 인가 : [Auth] http://www.313.co.kr/auth/
    
    NFS 관리 : [NAS] http://www.313.co.kr:5000/webman/index.cgi
    메일 관리 : [Mail] http://www.313.co.kr/mail/
    원격 관리 : [RDP] http://www.313.co.kr/guacamole/#/

--- arms/js/detail.js
+++ arms/js/detail.js
@@ -257,7 +257,7 @@ function scrollApiFunc() {
 
 // ------------------ 계정 정보 ------------------ //
 function bindAccountInfo() {
-    $("#user-name").html(userName);
+    $("#user-name").html(userName + "<span style=\"font-weight: normal\">님</span>");
     $("#user-name-detail").html(fullName + ' (' + userName + ')');
     $("#user-email").html(userEmail);
 }
@@ -440,6 +440,9 @@ function bindDataVersionTab() {
 
         $("#version-product-name").html(json.c_title);
         $("#version-accordion").jsonMenu(selectedPdServiceVersion, json.pdServiceVersionEntities, { speed: 5000 });
+        $(".version-list").slimscroll({
+            height: "512px"
+        });
 
         calledAPIs["versionAPI"] = true;
     });
@@ -473,7 +476,6 @@ function initVersionData() {
         // 버전 데이터 바인딩
         var element = $("a[data-value='" + c_id + "']");
         console.log("해당 요구사항의 버전 요소: ", element[0]);
-        element[0].style.background = "rgba(241, 240, 71, 0.3)";
 
         versionClick(element[0], c_id);
     };
@@ -483,8 +485,10 @@ function versionClick(element, c_id) {
     console.log("versionClick:: c_id  -> ", c_id);
     $("a[name='versionLink_List']").each(function () {
         this.style.background = "";
+        this.style.color = "";
     });
-    element.style.background = "rgba(241, 240, 71, 0.3)";
+    element.style.background = "rgba(2, 59, 109, 0.7)";
+    element.style.color = "rgb(255, 255, 255)";
     console.log(element);
 
     $(".spinner").html("<i class=\"fa fa-spinner fa-spin\"></i> 데이터를 로드 중입니다...");
