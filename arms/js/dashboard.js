366f4e686c777a8ce597d8b6bfceca3f49a15b87
jiyoondev <jiyoon.jang.dev@gmail.com>
2023-10-14 23:58:55 +0900

    feat: [ARMS-124] #comment 대시보드 제품-버전-작업자 생키 차트 생성 (mock) 23.10.14
    #close #time 10m +review SR @jiyoondev
    
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    요구 관리 : [Requirement Manage] http://www.a-rms.net
    문서 관리 : [Document] http://www.313.co.kr/confluence
    이슈 관리 : [IssueTracker] http://www.313.co.kr/jira
    소스 관리 : [VersionControl] https://github.com/313devgrp
    코드 리뷰 : [CodeReview] http://www.313.co.kr/fecru
    빌드 관리 : [CICD-Deploy] http://www.313.co.kr/jenkins
    배포 관리 : [Deploy-Pipeline] http://www.313.co.kr/spinnaker
    아티 팩트 : [ArtifactManager] http://www.313.co.kr/nexus
    코드 분석 : [CodeAnalysis] http://www.313.co.kr/sonar
    성능 측정 : [ZipKin] http://www.313.co.kr/zipkin/
    
    도커 관리 : [Docker Hub] https://hub.docker.com/u/313devgrp
    클러스터1 : [Kubernetes] http://www.313.co.kr:31380
    클러스터2 : [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    디비 관리 : [DB Admin] http://www.313.co.kr/phpmyadmin/
    오브 젝트 : [S3 Admin] http://www.313.co.kr/minio/login
    캐시 관리 : [Cache Admin] http://www.313.co.kr/redis/
    ELFK 스택1 : [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    ELFK 스택2 : [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    ELFK 스택3 : [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    ELFK 스택4 : [Kibana] http://www.313.co.kr/kibana/app/home#/
    
    모니터링 1 : [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    모니터링 2 : [Grafana] http://www.313.co.kr/grafana
    
    인증 인가 : [Auth] http://www.313.co.kr/auth/
    
    NFS 관리 : [NAS] http://www.313.co.kr:5000/webman/index.cgi
    메일 관리 : [Mail] http://www.313.co.kr/mail/
    원격 관리 : [RDP] http://www.313.co.kr/guacamole/#/

--- arms/js/dashboard.js
+++ arms/js/dashboard.js
@@ -44,7 +44,9 @@ function execDocReady() {
 			"../reference/lightblue4/docs/lib/slimScroll/jquery.slimscroll.min.js",
 			// 투입 인력별 요구사항 관여 차트
 			"../reference/jquery-plugins/Jit-2.0.1/jit.js",
-			"../reference/jquery-plugins/Jit-2.0.1/Examples/css/Treemap.css"],
+			"../reference/jquery-plugins/Jit-2.0.1/Examples/css/Treemap.css",
+			// 제품-버전-투입인력 차트
+			"../reference/jquery-plugins/d3-sankey-v0.12.3/d3-sankey.min.js"],
 
 		["../reference/jquery-plugins/dataTables-1.10.16/media/css/jquery.dataTables_lightblue4.css",
 			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Responsive/css/responsive.dataTables_lightblue4.css",
@@ -185,6 +187,10 @@ function makePdServiceSelectBox() {
 			drawManRequirementTreeMapChart(data);
 		});
 
+		// 제품-버전-투입인력 차트 mock 데이터 fetch
+		d3.json("./mock/productToMan.json", function (data) {
+			drawProductToManSankeyChart(data);
+		});
 	});
 } // end makePdServiceSelectBox()
 
@@ -793,3 +799,220 @@ function init(treeMapInfos) {
 		tm.out();
 	});
 }
+
+////////////////////////////////////////////////////////////////////////////////////////
+// 제품-버전-투입인력 차트 생성
+////////////////////////////////////////////////////////////////////////////////////////
+function drawProductToManSankeyChart(data) {
+	console.log("==== 장지윤 data");
+	console.log(data);
+	SankeyChart.loadChart(data);
+}
+
+var SankeyChart = (function ($) {
+	"use strict";
+
+	var initSvg = function () {
+		var margin = { top: 10, right: 10, bottom: 10, left: 10 };
+		var width = document.getElementById("chart-product-manpower").offsetWidth - margin.left - margin.right;
+		var height = 500 - margin.top - margin.bottom;
+
+		var vx = width + margin.left + margin.right;
+		var vy = height + margin.top + margin.bottom;
+
+		return d3
+			.select("#chart-product-manpower")
+			.append("svg")
+			.attr("viewBox", "0 0 " + vx + " " + vy)
+			.attr("width", width)
+			.attr("height", height)
+			.append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	};
+
+	var drawEmptyChart = function () {
+		var margin = { top: 10, right: 10, bottom: 10, left: 10 };
+		var width = document.getElementById("chart-product-manpower").offsetWidth - margin.left - margin.right;
+		var height = 500 - margin.top - margin.bottom;
+
+		initSvg()
+			.append("text")
+			.style("font-size", "12px")
+			.style("fill", "white")
+			.style("font-weight", 5)
+			.text("선택된 어플리케이션이 없습니다.")
+			.attr("x", width / 2)
+			.attr("y", height / 2);
+	};
+
+	var loadChart = function (data) {
+		var margin = { top: 10, right: 10, bottom: 10, left: 10 };
+		var width = document.getElementById("chart-product-manpower").offsetWidth - margin.left - margin.right;
+		var height = 500 - margin.top - margin.bottom;
+
+		var formatNumber = d3.format(",.0f");
+		var format = function (d) {
+			return formatNumber(d);
+		};
+
+		var iconXs = [10, 12, 11.5, 12];
+		var nodeIcons = ['<i class="fa fa-cube"></i>', '<i class="fa fa-server"></i>', '<i class="fa fa-database"></i>'];
+		var colors = ["#1f77b4", "#2ca02c", "#d62728"];
+
+		var svg = initSvg();
+
+		console.log("=== 장지윤 data");
+		console.log(data);
+
+		if (isEmpty(data.nodes)) {
+			svg
+				.append("text")
+				.style("font-size", "12px")
+				.style("fill", "white")
+				.style("font-weight", 5)
+				.text("해당 프로젝트에 매핑된 버전이 없습니다.")
+				.attr("x", width / 2)
+				.attr("y", height / 2);
+
+			return;
+		}
+
+		var sankey = d3.sankey().nodeWidth(36).nodePadding(40).size([width, height]);
+
+		var graph = {
+			nodes: [],
+			links: []
+		};
+		var nodeMap = { };
+
+		var color = d3.scaleOrdinal(colors);
+		var iconX = d3.scaleOrdinal(iconXs);
+		var nodeIcon = d3.scaleOrdinal(nodeIcons);
+
+		data.nodes.forEach(function (nodeInfos) {
+			graph.nodes.push(nodeInfos);
+		});
+
+		data.links.forEach(function (nodeLinks) {
+			graph.links.push(nodeLinks);
+		});
+
+		graph.nodes.forEach(function (node) {
+			nodeMap[node.id] = node;
+		});
+
+		graph.links = graph.links.map(function (link) {
+			return {
+				source: nodeMap[link.source],
+				target: nodeMap[link.target],
+				value: 1
+			};
+		});
+
+		graph = sankey(graph);
+
+		var link = svg
+			.append("g")
+			.selectAll(".link")
+			.data(graph.links)
+			.enter()
+			.append("path")
+			.attr("class", "link")
+			.attr("d", d3.sankeyLinkHorizontal())
+			.attr("stroke-width", function (d) {
+				return d.width;
+			});
+
+		link.append("title").text(function (d) {
+			return d.source.name + " → " + d.target.name + "\n" + format(d.value);
+		});
+
+		var node = svg.append("g").selectAll(".node").data(graph.nodes).enter().append("g").attr("class", "node");
+
+		node
+			.append("rect")
+			.attr("x", function (d) {
+				return d.x0;
+			})
+			.attr("y", function (d) {
+				return d.y0;
+			})
+			.attr("height", function (d) {
+				return d.y1 - d.y0;
+			})
+			.attr("width", sankey.nodeWidth())
+			.style("fill", function (d) {
+				return (d.color = color(d.type));
+			})
+			.style("stroke", function (d) {
+				return d3.rgb(d.color).darker(2);
+			})
+			.append("title")
+			.text(function (d) {
+				return d.name + "\n" + format(d.value);
+			});
+
+		node
+			.append("svg:foreignObject")
+			.attr("x", function (d) {
+				return d.x0 + iconX(d.type);
+			})
+			.attr("y", function (d) {
+				return (d.y1 + d.y0 - 16) / 2;
+			})
+			.attr("height", "16px")
+			.attr("width", "16px")
+			.html((d) => nodeIcon(d.type));
+
+		node
+			.append("text")
+			.attr("x", function (d) {
+				return d.x0 > 0 ? d.x0 - 6 : d.x1 - d.x0 + 6;
+			})
+			.attr("y", function (d) {
+				return (d.y1 + d.y0 - 18) / 2;
+			})
+			.attr("dy", "0.35em")
+			.style("fill", function (d) {
+				return (d.color = color(d.type));
+			})
+			.style("text-shadow", function (d) {
+				return `0 1px 0 ${(d.color = color(d.type))}`;
+			})
+			.attr("text-anchor", function (d) {
+				return d.x0 > 0 ? "end" : "start";
+			})
+			.text(function (d) {
+				return `[${d.type}]`;
+			})
+			.filter(function (d) {
+				return d.x < width / 2;
+			})
+			.attr("x", 6 + sankey.nodeWidth())
+			.attr("text-anchor", "start");
+
+		node
+			.append("text")
+			.attr("x", function (d) {
+				return d.x0 > 0 ? d.x0 - 6 : d.x1 - d.x0 + 6;
+			})
+			.attr("y", function (d) {
+				return (d.y1 + d.y0 + 18) / 2;
+			})
+			.attr("dy", ".35em")
+			.attr("text-anchor", function (d) {
+				return d.x0 > 0 ? "end" : "start";
+			})
+			.attr("transform", null)
+			.text(function (d) {
+				return d.name;
+			})
+			.filter(function (d) {
+				return d.x < width / 2;
+			})
+			.attr("x", 6 + sankey.nodeWidth())
+			.attr("text-anchor", "start");
+	};
+
+	return { loadChart, drawEmptyChart };
+})(jQuery);
