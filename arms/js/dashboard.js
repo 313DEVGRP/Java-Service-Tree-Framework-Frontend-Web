6fd04d9fd123d38cd97267f2f5d487180df4ad1e
yhs <gudtjr4662@gmail.com>
2023-10-29 00:37:15 +0900

    feat: [ARMSQA-23] #comment 대시보드 요구사항 미존재 관련 작성 - 23.10.29 #close #time 1h +review SR @HSYang-hub
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    요구 관리 : [Requirement Manage] http://www.a-rms.net
    문서 관리 : [Document] http://www.313.co.kr/confluence
    이슈 관리 : [IssueTracker] http://www.313.co.kr/jira
    소스 관리 : [VersionControl] https://github.com/313devgrp
    코드 리뷰 : [CodeReview] http://www.313.co.kr/fecru
    빌드 관리 : [CICD-Deploy] http://www.313.co.kr/jenkins
    배포 관리 : [Deploy-Pipeline] http://www.313.co.kr/spinnaker
    아티 팩트 : [ArtifactManager] http://www.313.co.kr/nexus
    코드 분석 : [CodeAnalysis] http://www.313.co.kr/sonar
    성능 측정 : [ZipKin] http://www.313.co.kr/zipkin/
    
    도커 관리 : [Docker Hub] https://hub.docker.com/u/313devgrp
    클러스터1 : [Kubernetes] http://www.313.co.kr:31380
    클러스터2 : [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    디비 관리 : [DB Admin] http://www.313.co.kr/phpmyadmin/
    오브 젝트 : [S3 Admin] http://www.313.co.kr/minio/login
    캐시 관리 : [Cache Admin] http://www.313.co.kr/redis/
    ELFK 스택1 : [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    ELFK 스택2 : [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    ELFK 스택3 : [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    ELFK 스택4 : [Kibana] http://www.313.co.kr/kibana/app/home#/
    
    모니터링 1 : [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    모니터링 2 : [Grafana] http://www.313.co.kr/grafana
    
    인증 인가 : [Auth] http://www.313.co.kr/auth/
    
    NFS 관리 : [NAS] http://www.313.co.kr:5000/webman/index.cgi
    메일 관리 : [Mail] http://www.313.co.kr/mail/
    원격 관리 : [RDP] http://www.313.co.kr/guacamole/#/

--- arms/js/dashboard.js
+++ arms/js/dashboard.js
@@ -10,7 +10,7 @@ var selectedIssueKey; //선택한 이슈 키
 
 var dashboardColor;
 var labelType, useGradients, nativeTextSupport, animate; //투입 인력별 요구사항 관여 차트
-var req_count, subtask_count, resource_count;
+var tot_ver_count, active_ver_count, req_count, subtask_count, resource_count;
 
 function execDocReady() {
 
@@ -390,9 +390,12 @@ function drawReqTimeSeries(data) {
 // 2. 버전만 따로 선택해서 보고싶은 경우(미완)
 function statisticsMonitor(pdservice_id, pdservice_version_id) {
 	console.log("선택된 서비스 ===> " + pdservice_id);
+	tot_ver_count = 0;
+	active_ver_count = 0;
 	req_count = 0;
 	subtask_count = 0;
 	resource_count = 0;
+
 	//1. 좌상 게이지 차트 및 타임라인
 	//2. Time ( 작업일정 ) - 버전 개수 삽입
 	d3.json("/auth-user/api/arms/pdService/getNodeWithVersionOrderByCidDesc.do?c_id=" + pdservice_id,function(json) {
@@ -401,6 +404,8 @@ function statisticsMonitor(pdservice_id, pdservice_version_id) {
 
 		let versionData = json.pdServiceVersionEntities;
 		let version_count = versionData.length;
+		tot_ver_count = version_count;
+
 		console.log("등록된 버전 개수 = " + version_count);
 		if(version_count !== undefined) {
 			$('#version_count').text(version_count);
@@ -451,6 +456,8 @@ function statisticsMonitor(pdservice_id, pdservice_version_id) {
 	setTimeout(function () {
 		//Scope - (2) 요구사항에 연결된 이슈 총 개수
 		getLinkedIssueCount(pdservice_id, ""); // 연결된 이슈 총 개수, 평균 값 대입
+
+		$('#inactive_version_count').text( tot_ver_count - active_ver_count );
 	},1000);
 
 	//drawBarOnPolar("polar_bar", categories_mock, data_mock);
@@ -1554,7 +1561,10 @@ function getReqCount(pdservice_id, pdServiceVersionLinks) {
 					console.log(key + "=" + value);
 				}
 				//해당 제품의 총 요구사항 수 (by db)
-				$('#active_version_count').text(data["version"]);
+				active_ver_count = data["version"];
+				$('#active_version_count').text(active_ver_count);
+				console.log("active_ver_count = " + active_ver_count);
+
 				$('#req_count').text(data["req"]);
 				if(data["req"] == "" || data["req"] == 0) {
 					req_count = -1;
