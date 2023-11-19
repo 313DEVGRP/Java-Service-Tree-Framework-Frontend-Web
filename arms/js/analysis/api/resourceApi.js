126fb276ada1d9ccefb28c45df952d00a787f98a
yhs <gudtjr4662@gmail.com>
2023-11-19 22:07:29 +0900

    feat: [ARMS-158] #comment 리소스분석 - 리소스_작업자별_상태, 버전별 필터, 데이터테이블 컴포넌트화  - 23.11.19 #close #time 1h +review SR @HSYang-hub
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    요구 관리 : [Requirement Manage] http://www.a-rms.net
    문서 관리 : [Document] http://www.313.co.kr/confluence
    이슈 관리 : [IssueTracker] http://www.313.co.kr/jira
    소스 관리 : [VersionControl] https://github.com/313devgrp
    코드 리뷰 : [CodeReview] http://www.313.co.kr/fecru
    빌드 관리 : [CICD-Deploy] http://www.313.co.kr/jenkins
    배포 관리 : [Deploy-Pipeline] http://www.313.co.kr/spinnaker
    아티 팩트 : [ArtifactManager] http://www.313.co.kr/nexus
    코드 분석 : [CodeAnalysis] http://www.313.co.kr/sonar
    성능 측정 : [ZipKin] http://www.313.co.kr/zipkin/
    
    도커 관리 : [Docker Hub] https://hub.docker.com/u/313devgrp
    클러스터1 : [Kubernetes] http://www.313.co.kr:31380
    클러스터2 : [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    디비 관리 : [DB Admin] http://www.313.co.kr/phpmyadmin/
    오브 젝트 : [S3 Admin] http://www.313.co.kr/minio/login
    캐시 관리 : [Cache Admin] http://www.313.co.kr/redis/
    ELFK 스택1 : [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    ELFK 스택2 : [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    ELFK 스택3 : [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    ELFK 스택4 : [Kibana] http://www.313.co.kr/kibana/app/home#/
    
    모니터링 1 : [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    모니터링 2 : [Grafana] http://www.313.co.kr/grafana
    
    인증 인가 : [Auth] http://www.313.co.kr/auth/
    
    NFS 관리 : [NAS] http://www.313.co.kr:5000/webman/index.cgi
    메일 관리 : [Mail] http://www.313.co.kr/mail/
    원격 관리 : [RDP] http://www.313.co.kr/guacamole/#/

---
+++ arms/js/analysis/api/resourceApi.js
@@ -0,0 +1,105 @@
+var ResourceApi = (function () {
+    var selectedPdserviceId;
+    var selectedPdServiceversions = [];
+
+    var fetchedResourceData = [];
+    var fetchedResourceDetailData = [];
+
+    var setPdServiceId = function (pdServiceId) { selectedPdserviceId = pdServiceId; }
+    var getPdServiceId = function () { return selectedPdserviceId; }
+
+    var setPdServiceVersionLinks = function (pdServiceVersionLinks) {
+        selectedPdServiceversions = pdServiceVersionLinks; }
+    var getPdServiceVersions = function () { return selectedPdServiceversions };
+
+    var fetchResourceData = function (pdservice_id, pdServiceVersionLinks) {
+        var deferred = $.Deferred();
+        setPdServiceId(pdservice_id);
+        setPdServiceVersionLinks(pdServiceVersionLinks);
+
+        $.ajax({
+            url: "/auth-user/api/arms/analysis/resource/workerStatus/"+pdservice_id,
+            type: "GET",
+            data: { "서비스아이디" : pdservice_id,
+                //"메인그룹필드" : "assignee.assignee_displayName.keyword",
+                "메인그룹필드" : "assignee.assignee_emailAddress.keyword",
+                "하위그룹필드들": "isReq,status.status_name.keyword",
+                "컨텐츠보기여부" : true,
+                "크기" : 1000,
+                "하위크기": 1000,
+                "pdServiceVersionLinks" : pdServiceVersionLinks},
+            contentType: "application/json;charset=UTF-8",
+            dataType: "json",
+            progress: true,
+            statusCode: {
+                200: function (data) {
+                    //console.log("=== === === 작업자 상태 집계 시작=== === ===")
+                    console.log(data);
+                    let search_name = data["검색결과"]["group_by_assignee.assignee_displayName.keyword"];
+                    let search_mail = data["검색결과"]["group_by_assignee.assignee_emailAddress.keyword"];
+                    //console.log("=== === === 작업자 상태 집계 종료=== === ===")
+                    fetchedResourceData = search_mail;
+                    deferred.resolve(fetchedResourceData);
+                },
+                error: function (e) {
+                    jError("Resource Status 조회에 실패했습니다. 나중에 다시 시도 바랍니다.");
+                }
+            }
+        });
+        return deferred.promise();
+    }
+    var getFetchedResourceData = function () {
+        return fetchedResourceData;
+    }
+
+    var fetchResourceDetailInfo = function (assignee_email) {
+        var deferred = $.Deferred();
+        let pdservice_id = getPdServiceId();
+        let pdServiceVersionLinks = getPdServiceVersions();
+        $.ajax({
+            url: "/auth-user/api/arms/analysis/resource/workerStatus/"+pdservice_id,
+            type: "GET",
+            data: { "서비스아이디" : pdservice_id,
+                //"메인그룹필드" : "assignee.assignee_displayName.keyword",
+                "메인그룹필드" : "assignee.assignee_emailAddress.keyword",
+                "하위그룹필드들": "isReq,status.status_name.keyword",
+                "컨텐츠보기여부" : true,
+                "크기" : 1000,
+                "하위크기": 1000,
+                "pdServiceVersionLinks" : pdServiceVersionLinks},
+            contentType: "application/json;charset=UTF-8",
+            dataType: "json",
+            progress: true,
+            statusCode: {
+                200: function (data) {
+                    console.log("=== === === 작업자 상태 집계 시작=== === ===")
+                    console.log(data);
+                    let search_keys1 = data["검색결과"]["group_by_assignee.assignee_displayName.keyword"];
+                    let search_keys2 = data["검색결과"]["group_by_assignee.assignee_emailAddress.keyword"];
+                    console.log(search_keys1);
+                    console.log("=== === === 작업자 상태 집계 종료=== === ===")
+
+                    //fetchedResourceData = data["검색결과"]["group_by_assignee.assignee_displayName.keyword"];
+                    fetchedResourceDetailData = search_keys2;
+                    deferred.resolve(fetchedResourceDetailData);
+                },
+                error: function (e) {
+                    jError("Resource Status 조회에 실패했습니다. 나중에 다시 시도 바랍니다.");
+                }
+            }
+        });
+
+        return deferred.promise();
+    }
+
+    var getFetchedResourceDetailData = function () {
+        return fetchedResourceDetailData;
+    }
+
+    return {
+        setPdServiceId, getPdServiceId,
+        setPdServiceVersionLinks, getPdServiceVersions,
+        fetchResourceData, getFetchedResourceData,
+        fetchResourceDetailInfo, getFetchedResourceDetailData
+    }
+})();
