c4e736c93c6951941915033802c546c564364bc3
LeeDongMin <admin@313.co.kr>
2023-11-27 21:01:50 +0900

    feat: [ARMSQA-19] #comment :white_check_mark: 인덱스개편 - 23.11.26 #close #time 1h +review SR @313devops
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    요구 관리 : [Requirement Manage] http://www.a-rms.net
    문서 관리 : [Document] http://www.313.co.kr/confluence
    이슈 관리 : [IssueTracker] http://www.313.co.kr/jira
    소스 관리 : [VersionControl] https://github.com/313devgrp
    코드 리뷰 : [CodeReview] http://www.313.co.kr/fecru
    빌드 관리 : [CICD-Deploy] http://www.313.co.kr/jenkins
    배포 관리 : [Deploy-Pipeline] http://www.313.co.kr/spinnaker
    아티 팩트 : [ArtifactManager] http://www.313.co.kr/nexus
    코드 분석 : [CodeAnalysis] http://www.313.co.kr/sonar
    성능 측정 : [ZipKin] http://www.313.co.kr/zipkin/
    
    도커 관리 : [Docker Hub] https://hub.docker.com/u/313devgrp
    클러스터1 : [Kubernetes] http://www.313.co.kr:31380
    클러스터2 : [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    디비 관리 : [DB Admin] http://www.313.co.kr/phpmyadmin/
    오브 젝트 : [S3 Admin] http://www.313.co.kr/minio/login
    캐시 관리 : [Cache Admin] http://www.313.co.kr/redis/
    ELFK 스택1 : [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    ELFK 스택2 : [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    ELFK 스택3 : [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    ELFK 스택4 : [Kibana] http://www.313.co.kr/kibana/app/home#/
    
    모니터링 1 : [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    모니터링 2 : [Grafana] http://www.313.co.kr/grafana
    
    인증 인가 : [Auth] http://www.313.co.kr/auth/
    
    NFS 관리 : [NAS] http://www.313.co.kr:5000/webman/index.cgi
    메일 관리 : [Mail] http://www.313.co.kr/mail/
    원격 관리 : [RDP] http://www.313.co.kr/guacamole/#/

--- arms/js/index.js
+++ arms/js/index.js
@@ -13,72 +13,79 @@ gsap.set(outerWrappers, { yPercent: 100 });
 gsap.set(innerWrappers, { yPercent: -100 });
 
 function gotoSection(index, direction) {
-	animating = true;
+  animating = true;
 
-	const fromTop = direction === -1;
-	const dFactor = fromTop ? -1 : 1;
-	const tl = gsap.timeline({
-		defaults: { duration: 1, ease: 'power1.inOut' },
-		onComplete: () => (animating = false),
-	});
+  const fromTop = direction === -1;
+  const dFactor = fromTop ? -1 : 1;
+  const tl = gsap.timeline({
+    defaults: { duration: 1, ease: 'power1.inOut' },
+    onComplete: () => (animating = false),
+  });
 
-	if (currentIndex >= 0) {
-		gsap.set(sections[currentIndex], { zIndex: 0 });
+  if (currentIndex >= 0) {
+    gsap.set(sections[currentIndex], { zIndex: 0 });
 
-		tl.to(images[currentIndex], { yPercent: -15 * dFactor })
-			.set(sections[currentIndex], { autoAlpha: 0 })
-			.fromTo(
-				images[currentIndex],
-				{ scale: 1, autoAlpha: 1 },
-				{ scale: 0.5, autoAlpha: 0 },
-				0
-			);
+    tl.to(images[currentIndex], { yPercent: -15 * dFactor })
+      .set(sections[currentIndex], { autoAlpha: 0 })
+      .fromTo(
+        images[currentIndex],
+        { scale: 1, autoAlpha: 1 },
+        { scale: 0.5, autoAlpha: 0 },
+        0
+      );
 
-		dots[currentIndex].classList.remove('active');
-	}
+    dots[currentIndex].classList.remove('active');
+  }
 
-	gsap.set(sections[index], { autoAlpha: 1, zIndex: 1 });
-	gsap.set(images[index], { scale: 1, autoAlpha: 1 });
+  gsap.set(sections[index], { autoAlpha: 1, zIndex: 1 });
+  gsap.set(images[index], { scale: 1, autoAlpha: 1 });
 
-	tl.fromTo(
-		[outerWrappers[index], innerWrappers[index]],
-		{
-			yPercent: (i) => (i ? -100 * dFactor : 100 * dFactor),
-		},
-		{
-			yPercent: 0,
-		},
-		0
-	).fromTo(images[index], { yPercent: 15 * dFactor }, { yPercent: 0 }, 0);
+  tl.fromTo(
+    [outerWrappers[index], innerWrappers[index]],
+    {
+      yPercent: (i) => (i ? -100 * dFactor : 100 * dFactor),
+    },
+    {
+      yPercent: 0,
+    },
+    0
+  ).fromTo(images[index], { yPercent: 15 * dFactor }, { yPercent: 0 }, 0);
 
-	dots[index].classList.add('active');
+  dots[index].classList.add('active');
 
-	currentIndex = index;
+  currentIndex = index;
 }
 
 Observer.create({
-	type: 'wheel,touch,pointer',
-	wheelSpeed: -1,
-	onDown: () => {
-		if (animating) return;
-		if (currentIndex - 1 < 0) return;
+  type: 'wheel,touch,pointer',
+  wheelSpeed: -1,
+  onDown: () => {
+    if (animating) return;
+    if (currentIndex - 1 < 0) return;
+    if (sections[currentIndex].querySelector('.inner').scrollTop > 0) return;
 
-		gotoSection(currentIndex - 1, -1);
-	},
-	onUp: () => {
-		if (animating) return;
-		if (currentIndex + 1 >= sections.length) return;
+    gotoSection(currentIndex - 1, -1);
+  },
+  onUp: () => {
+    if (animating) return;
+    if (currentIndex + 1 >= sections.length) return;
+    if (
+      sections[currentIndex].querySelector('.inner').scrollTop +
+        sections[currentIndex].querySelector('.inner').offsetHeight <
+      sections[currentIndex].querySelector('.inner').scrollHeight
+    )
+      return;
 
-		gotoSection(currentIndex + 1, 1);
-	},
-	tolerance: 10,
-	preventDefault: true,
+    gotoSection(currentIndex + 1, 1);
+  },
+  tolerance: 10,
+  preventDefault: false,
 });
 
 [].forEach.call(document.querySelectorAll('.btn'), (btn, i) => {
-	btn.addEventListener('click', () =>
-		gotoSection(i, currentIndex > i ? -1 : 1)
-	);
+  btn.addEventListener('click', () =>
+    gotoSection(i, currentIndex > i ? -1 : 1)
+  );
 });
 
 gotoSection(0, 1);
