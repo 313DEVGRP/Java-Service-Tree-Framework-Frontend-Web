f4c1fdaa452cb8fbce1019e5ff62ec5c586b8baa
함수원 <dev.code.su@gmail.com>
2023-12-04 10:45:35 +0900

    ✨ [MOD] detail template migration
    - base template
    - data bind => detail_per_version && detail_total_reqadd

--- arms/js/detail_per_version.js
+++ arms/js/detail_per_version.js
@@ -1,35 +1,206 @@
 ////////////////////////////////////////////////////////////////////////////////////////
-//Document Ready ( execArmsDocReady )
+//Page 전역 변수
 ////////////////////////////////////////////////////////////////////////////////////////
+var selectId; // 제품 아이디
+var selectName; // 제품 이름
+var selectedIndex; // 데이터테이블 선택한 인덱스
+var selectedPage; // 데이터테이블 선택한 인덱스
+var selectVersion; // 선택한 버전 아이디
+var dataTableRef; // 데이터테이블 참조 변수
 
-
-
-// 절대로 armsDetailExceptTemplate 폴더안에 있는 파일 사용하지 마세요
-// armsDetailExceptTemplate 폴더를 삭제할 예정입니다.
-
-
-
+////////////////////////////////////////////////////////////////////////////////////////
+//Document Ready
+////////////////////////////////////////////////////////////////////////////////////////
 function execDocReady() {
+	var pluginGroups = [
+		[
+			"../reference/light-blue/lib/bootstrap-datepicker.js",
+			"../reference/jquery-plugins/datetimepicker-2.5.20/build/jquery.datetimepicker.min.css",
+			"../reference/jquery-plugins/datetimepicker-2.5.20/build/jquery.datetimepicker.full.min.js",
+			"../reference/lightblue4/docs/lib/widgster/widgster.js"
+		],
 
-    var pluginGroups = [
-        [	"../reference/light-blue/lib/vendor/jquery.ui.widget.js",
-            "../reference/lightblue4/docs/lib/widgster/widgster.js"]
-        // 추가적인 플러그인 그룹들을 이곳에 추가하면 됩니다.
-    ];
+		[
+			"../reference/jquery-plugins/select2-4.0.2/dist/css/select2_lightblue4.css",
+			"../reference/jquery-plugins/lou-multi-select-0.9.12/css/multiselect-lightblue4.css",
+			"../reference/jquery-plugins/multiple-select-1.5.2/dist/multiple-select-bluelight.css",
+			"../reference/jquery-plugins/select2-4.0.2/dist/js/select2.min.js",
+			"../reference/jquery-plugins/lou-multi-select-0.9.12/js/jquery.quicksearch.js",
+			"../reference/jquery-plugins/lou-multi-select-0.9.12/js/jquery.multi-select.js",
+			"../reference/jquery-plugins/multiple-select-1.5.2/dist/multiple-select.min.js"
+		],
 
-    loadPluginGroupsParallelAndSequential(pluginGroups)
-        .then(function() {
+		[
+			"../reference/jquery-plugins/dataTables-1.10.16/media/css/jquery.dataTables_lightblue4.css",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Responsive/css/responsive.dataTables_lightblue4.css",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Select/css/select.dataTables_lightblue4.css",
+			"../reference/jquery-plugins/dataTables-1.10.16/media/js/jquery.dataTables.min.js",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Responsive/js/dataTables.responsive.min.js",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Select/js/dataTables.select.min.js",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/RowGroup/js/dataTables.rowsGroup.min.js",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/dataTables.buttons.min.js",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/buttons.html5.js",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/buttons.print.js",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/jszip.min.js",
+			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/pdfmake.min.js"
+		]
+		// 추가적인 플러그인 그룹들을 이곳에 추가하면 됩니다.
+	];
 
-            console.log('모든 플러그인 로드 완료');
+	loadPluginGroupsParallelAndSequential(pluginGroups)
+		.then(function () {
+			//vfs_fonts 파일이 커서 defer 처리 함.
+			setTimeout(function () {
+				var script = document.createElement("script");
+				script.src = "../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/vfs_fonts.js";
+				script.defer = true; // defer 속성 설정
+				document.head.appendChild(script);
+			}, 3000); // 2초 후에 실행됩니다.
+			console.log("모든 플러그인 로드 완료");
 
-            //좌측 메뉴
-            $('.widget').widgster();
-            setSideMenu("sidebar_menu_dashboard", "");
-            // 스크립트 실행 로직을 이곳에 추가합니다.
+			//사이드 메뉴 처리
+			$(".widget").widgster();
+			setSideMenu("sidebar_menu_product", "sidebar_menu_version_manage");
 
-        })
-        .catch(function() {
-            console.error('플러그인 로드 중 오류 발생');
-        });
+			// 스크립트 실행 로직을 이곳에 추가합니다.
 
+			init_versionList();
+			dataLoad(22, "313 SOFT - 요구사항 관리 시스템 - ALM RMS");
+		})
+		.catch(function () {
+			console.error("플러그인 로드 중 오류 발생");
+		});
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//버전 리스트를 재로드하는 함수 ( 버전 추가, 갱신, 삭제 시 호출 )
+////////////////////////////////////////////////////////////////////////////////////////
+function dataLoad(getSelectedText, selectedText) {
+	// ajax 처리 후 에디터 바인딩.
+	console.log("dataLoad :: getSelectedID → " + getSelectedText);
+	$.ajax("/auth-user/api/arms/pdService/getNodeWithVersionOrderByCidDesc.do?c_id=" + getSelectedText).done(function (
+		json
+	) {
+		console.log("dataLoad :: success → ", json);
+		$("#version_accordion").jsonMenu("set", json.pdServiceVersionEntities, { speed: 5000 });
+		//version text setting
+
+		$("#select_Service").text(selectedText); // sender 이름 바인딩
+
+		if (!isEmpty(json.pdServiceVersionEntities)) {
+			// 상세보기
+			selectVersion = json.pdServiceVersionEntities[0].c_id;
+			$("#version_start_date").val(json.pdServiceVersionEntities[0].c_start_date);
+			$("#version_end_date").val(json.pdServiceVersionEntities[0].c_end_date);
+			$("#version_contents").html(json.pdServiceVersionEntities[0].c_contents);
+		}
+	});
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+// versionlist 이니셜라이즈
+////////////////////////////////////////////////////////////////////////////////////////
+function init_versionList() {
+	var menu;
+	$.fn.jsonMenu = function (action, items, options) {
+		$(this).addClass("json-menu");
+		if (action == "add") {
+			menu.body.push(items);
+			draw($(this), menu);
+		} else if (action == "set") {
+			menu = items;
+			// $("#select_Version").text(items[0].c_title);  // 로드시 첫번째 버전
+			draw($(this), menu);
+		}
+		return this;
+	};
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//version list html 삽입
+////////////////////////////////////////////////////////////////////////////////////////
+function draw(main, menu) {
+	main.html("");
+
+	var data = "";
+	for (var i = 0; i < menu.length; i++) {
+		if (i == 0) {
+			data += `
+			   <div class="panel">
+				   <div class="panel-heading">
+					   <a class="accordion-toggle collapsed"
+					   			data-toggle="collapse"
+					   			name="versionLink_List"
+					   			style="color: #a4c6ff; text-decoration: none; cursor: pointer;  "
+					   			onclick="versionClick(this, ${menu[i].c_id});
+					   			return false;">
+						   ${menu[i].c_title}
+					   </a>
+				   </div>
+			   </div>`;
+		} else {
+			data += `
+			   <div class="panel">
+				   <div class="panel-heading">
+					   <a class="accordion-toggle collapsed"
+					   			data-toggle="collapse"
+					   			name="versionLink_List"
+					   			style="color: #a4c6ff; text-decoration: none; cursor: pointer;"
+					   			onclick="versionClick(this, ${menu[i].c_id});
+					   			return false;">
+						   ${menu[i].c_title}
+					   </a>
+				   </div>
+			   </div>`;
+		}
+	}
+
+	main.html(data);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//버전 클릭할 때 동작하는 함수
+//1. 상세보기 데이터 바인딩
+//2. 편집하기 데이터 바인딩
+////////////////////////////////////////////////////////////////////////////////////////
+function versionClick(element, c_id) {
+	console.log("versionClick:: c_id  -> ", c_id);
+	$("a[name='versionLink_List']").each(function () {
+		this.style.background = "";
+	});
+	element.style.background = "rgba(229, 96, 59, 0.3)";
+	console.log(element);
+
+	selectVersion = c_id;
+
+	$.ajax({
+		url: "/auth-user/api/arms/pdServiceVersion/getNode.do", // 클라이언트가 HTTP 요청을 보낼 서버의 URL 주소
+		data: { c_id: c_id }, // HTTP 요청과 함께 서버로 보낼 데이터
+		method: "GET", // HTTP 요청 메소드(GET, POST 등)
+		dataType: "json" // 서버에서 보내줄 데이터의 타입
+	})
+		// HTTP 요청이 성공하면 요청한 데이터가 done() 메소드로 전달됨.
+		.done(function (json) {
+			console.log(" → " + json.c_contents);
+			console.log(json);
+
+			$("#select_Version").val(json.c_title);
+			$("#version_start_date").val(json.c_pds_version_start_date);
+			$("#version_end_date").val(json.c_pds_version_end_date);
+			$("#version_contents").html(json.c_pds_version_contents);
+
+			// sender 데이터 바인딩 및 선택 색상 표기
+			$("#select_Version").text(json.c_title);
+			$(".list-item1 .chat-message-body").css({ "border-left": "" });
+			$(".list-item1 .arrow").css({ "border-right": "" });
+		})
+		// HTTP 요청이 실패하면 오류와 상태에 관한 정보가 fail() 메소드로 전달됨.
+		.fail(function (xhr, status, errorThrown) {
+			console.log(xhr + status + errorThrown);
+		})
+		//
+		.always(function (xhr, status) {
+			$("#text").html("요청이 완료되었습니다!");
+			console.log(xhr + status);
+		});
 }
