d2347deffd142aa371e8f770a8412c357196a5c6
dumbbelloper <dumbbelloper@gmail.com>
2023-12-06 02:31:23 +0900

    feat: [ARMS-233] #comment 트리맵 차트 라이브러리 변경 (Apache Echarts) - 23.12.06
    1. API 데이터 없을 경우 No Data 표기하는 부분 이슈 발견되어 제거. 익일 작업 예정
    2. 사용하지 않는 json 파일(mock data) 삭제
    
    #close #time 3h +review SR @dumbbelloper
    
    See http://www.313.co.kr/jira/browse/ARMS-233 more details.
    
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    요구 관리 : [Requirement Manage] http://www.a-rms.net
    문서 관리 : [Document] http://www.313.co.kr/confluence
    이슈 관리 : [IssueTracker] http://www.313.co.kr/jira
    소스 관리 : [VersionControl] https://github.com/313devgrp
    코드 리뷰 : [CodeReview] http://www.313.co.kr/fecru
    빌드 관리 : [CICD-Deploy] http://www.313.co.kr/jenkins
    배포 관리 : [Deploy-Pipeline] http://www.313.co.kr/spinnaker
    아티 팩트 : [ArtifactManager] http://www.313.co.kr/nexus
    코드 분석 : [CodeAnalysis] http://www.313.co.kr/sonar
    성능 측정 : [ZipKin] http://www.313.co.kr/zipkin/
    
    도커 관리 : [Docker Hub] https://hub.docker.com/u/313devgrp
    클러스터1 : [Kubernetes] http://www.313.co.kr:31380
    클러스터2 : [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    디비 관리 : [DB Admin] http://www.313.co.kr/phpmyadmin/
    오브 젝트 : [S3 Admin] http://www.313.co.kr/minio/login
    캐시 관리 : [Cache Admin] http://www.313.co.kr/redis/
    ELFK 스택1 : [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    ELFK 스택2 : [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    ELFK 스택3 : [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    ELFK 스택4 : [Kibana] http://www.313.co.kr/kibana/app/home#/
    
    모니터링 1 : [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    모니터링 2 : [Grafana] http://www.313.co.kr/grafana
    
    인증 인가 : [Auth] http://www.313.co.kr/auth/
    
    NFS 관리 : [NAS] http://www.313.co.kr:5000/webman/index.cgi
    메일 관리 : [Mail] http://www.313.co.kr/mail/
    원격 관리 : [RDP] http://www.313.co.kr/guacamole/#/

--- arms/js/dashboard.js
+++ arms/js/dashboard.js
@@ -53,9 +53,6 @@ function execDocReady() {
 			"../reference/jquery-plugins/datetimepicker-2.5.20/build/jquery.datetimepicker.full.min.js",
 			"../reference/lightblue4/docs/lib/widgster/widgster.js",
 			"../reference/lightblue4/docs/lib/slimScroll/jquery.slimscroll.min.js",
-			// 투입 인력별 요구사항 관여 차트
-			"../reference/jquery-plugins/Jit-2.0.1/jit.js",
-			"../reference/jquery-plugins/Jit-2.0.1/Examples/css/Treemap.css",
 			// 제품-버전-투입인력 차트
 			"../reference/jquery-plugins/d3-sankey-v0.12.3/d3-sankey.min.js"],
 
@@ -671,10 +668,11 @@ function drawManRequirementTreeMapChart(pdServiceLink, pdServiceVersionLinks) {
 		.addQueryParam('pdServiceVersionLinks', pdServiceVersionLinks)
 		.addQueryParam('메인그룹필드', "pdServiceVersion")
 		.addQueryParam('하위그룹필드들', "assignee.assignee_accountId.keyword,assignee.assignee_displayName.keyword")
-		.addQueryParam('크기', 5)
+		.addQueryParam('크기', 0)
 		.addQueryParam('하위크기', 0)
 		.addQueryParam('컨텐츠보기여부', true)
 		.build();
+
 	$.ajax({
 		url: url,
 		type: "GET",
@@ -683,181 +681,109 @@ function drawManRequirementTreeMapChart(pdServiceLink, pdServiceVersionLinks) {
 		progress: true,
 		statusCode: {
 			200: function (data) {
-				if ($("#chart-manpower-requirement").children().length !== 0) {
-					$("#chart-manpower-requirement").empty();
+				let chartDom = document.getElementById('chart-manpower-requirement');
+
+				let myChart = echarts.init(chartDom);
+
+				let option;
+
+				myChart.showLoading();
+				myChart.hideLoading();
+
+				function getLevelOption() {
+					return [
+						{
+							itemStyle: {
+								borderColor: '#777',
+								borderWidth: 0,
+								gapWidth: 1
+							},
+							upperLabel: {
+								show: false
+							}
+						},
+						{
+							itemStyle: {
+								borderColor: '#555',
+								borderWidth: 5,
+								gapWidth: 1
+							},
+							emphasis: {
+								itemStyle: {
+									borderColor: '#ddd'
+								}
+							}
+						},
+						{
+							colorSaturation: [0.35, 0.5],
+							itemStyle: {
+								borderWidth: 5,
+								gapWidth: 1,
+								borderColorSaturation: 0.6
+							}
+						}
+					];
 				}
-				const treeMapInfos = {
-					"id": "root",
-					"name": "작업자별 요구사항 관여 트리맵",
-					"data": {},
-					"children": data
-				};
-				init(treeMapInfos);
+
+				myChart.setOption(
+					(option = {
+						// title: {
+						// 	text: '작업자 별 요구사항 관여 트리맵',
+						// 	left: 'center',
+						// 	textStyle: {
+						// 		color: '#ffffff'
+						// 	}
+						// },
+						tooltip: {
+							formatter: function (info) {
+								var value = info.value;
+								var treePathInfo = info.treePathInfo;
+								var treePath = [];
+								for (var i = 1; i < treePathInfo.length; i++) {
+									treePath.push(treePathInfo[i].name);
+								}
+								return [
+									'<div class="tooltip-title">' +
+									echarts.format.encodeHTML(treePath.join('/')) +
+									'</div>',
+									'관여한 횟수: ' + echarts.format.addCommas(value)
+								].join('');
+							}
+						},
+						series: [
+							{
+								name: '작업자 별 요구사항 관여 트리맵',
+								type: 'treemap',
+								breadcrumb: {
+									itemStyle: {
+										// color: '#90ee90'
+										// color: '#add8e6'
+									}
+								},
+								visibleMin: 300,
+								label: {
+									show: true,
+									formatter: '{b}'
+								},
+								upperLabel: {
+									show: true,
+									height: 30
+								},
+								itemStyle: {
+									borderColor: '#fff'
+								},
+								levels: getLevelOption(),
+								data: data
+							}
+						]
+					})
+				);
+				option && myChart.setOption(option);
+				window.addEventListener('resize', function () {
+					myChart.resize();
+				});
 			}
 		}
-	});
-}
-
-(function () {
-	var ua = navigator.userAgent,
-		iStuff = ua.match(/iPhone/i) || ua.match(/iPad/i),
-		typeOfCanvas = typeof HTMLCanvasElement,
-		nativeCanvasSupport = typeOfCanvas == "object" || typeOfCanvas == "function",
-		textSupport =
-			nativeCanvasSupport && typeof document.createElement("canvas").getContext("2d").fillText == "function";
-	//I'm setting this based on the fact that ExCanvas provides text support for IE
-	//and that as of today iPhone/iPad current text support is lame
-	labelType = !nativeCanvasSupport || (textSupport && !iStuff) ? "Native" : "HTML";
-	nativeTextSupport = labelType == "Native";
-	useGradients = nativeCanvasSupport;
-	animate = !(iStuff || !nativeCanvasSupport);
-})();
-
-var Log = {
-	elem: false,
-	write: function (text) {
-		if (!this.elem) this.elem = document.getElementById("log");
-		this.elem.innerHTML = text;
-		this.elem.style.left = 500 - this.elem.offsetWidth / 2 + "px";
-	}
-};
-
-function init(treeMapInfos) {
-	const colorMapping = {};
-	// TODO: 요구사항의 가짓수가 많아지면 색상을 더 추가해야 함
-	let colors = dashboardColor.treeMapColor;
-	function getColorForName(name) {
-		if (!colorMapping[name]) {
-			const selectedColor = colors.shift();
-			colorMapping[name] = selectedColor;
-			colors = colors.filter(color => color !== selectedColor);
-		}
-		return colorMapping[name];
-	}
-
-	treeMapInfos.children.forEach((worker) => {
-		worker.children.forEach((task) => {
-			const color = getColorForName(task.name);
-			task.data.$color = color;
-			task.id = task.id + "-" + worker.id;
-			task.data.$area = task.data.involvedCount;
-		});
-		worker.data.$area = worker.data.totalInvolvedCount;
-	});
-	//init TreeMap
-	var tm = new $jit.TM.Squarified({
-		//where to inject the visualization
-		injectInto: "chart-manpower-requirement",
-		//parent box title heights
-		titleHeight: 22,
-		//enable animations
-		animate: animate,
-		//box offsets
-		offset: 2.5,
-		//Attach left and right click events
-		Events: {
-			enable: true,
-			onClick: function (node) {
-				if (node) tm.enter(node);
-			},
-			onRightClick: function () {
-				tm.out();
-			}
-		},
-		duration: 300,
-		//Enable tips
-		Tips: {
-			enable: true,
-			//add positioning offsets
-			offsetX: 20,
-			offsetY: 20,
-			//implement the onShow method to
-			//add content to the tooltip when a node
-			//is hovered
-			onShow: function (tip, node, isLeaf, domElement) {
-				var html =
-					'<div class="tip-title" style="font-size: 13px; font-weight: bolder">' +
-					node.name +
-					'</div><div class="tip-text">';
-				var data = node.data;
-				if (data.involvedCount) {
-					html +=
-						"<div style='white-space: pre-wrap; font-size: 11px;'>관여한 횟수 : <span style='color: lawngreen'>" +
-						data.involvedCount +
-						"<br/></span></div>";
-				}
-				if (data.totalInvolvedCount) {
-					html +=
-						"<div style='white-space: pre-wrap; font-size: 11px;'>작업자가 관여한 총 횟수 : <span style='color: orange'>" +
-						data.totalInvolvedCount +
-						"<br/></span></div>";
-				}
-				if (data.image) {
-					html += '<img src="' + data.image + '" class="album" />';
-				}
-				html +=
-					"<div style='white-space: pre-wrap; margin-top: 8px; color: #d0d0d0; font-size: 10px'>우클릭 시 뒤로 갑니다.<br/></div>";
-				tip.innerHTML = html;
-			}
-		},
-		//Add the name of the node in the correponding label
-		//This method is called once, on label creation.
-		onCreateLabel: function (domElement, node) {
-			if (node.id === "root" || !node.id.includes("app")) {
-				var html =
-					'<div style="font-size: 13px; font-weight: bolder; text-align: center; margin: 2.5px 0 0 0">' +
-					node.name +
-					"</div>";
-			} else {
-				var html =
-					'<div style="font-size: 15.5px; font-weight: 600; text-align: center; margin: 2.5px 0 0 0; color: #464649">' +
-					// '<span style="color: whitesmoke; -webkit-text-stroke: 0.1px #1A2920;">' +
-					node.name +
-					// "</span>" +
-					"</div>";
-			}
-			domElement.innerHTML = html;
-			var style = domElement.style;
-			style.display = "";
-			style.border = "1.5px solid transparent";
-			// style.borderRadius = "50%";
-			domElement.onmouseover = function () {
-				style.border = "1.5px solid #9FD4FF";
-			};
-			domElement.onmouseout = function () {
-				style.border = "1.5px solid transparent";
-			};
-		}
-	});
-	tm.loadJSON(treeMapInfos);
-	tm.refresh();
-	//end
-	//add events to radio buttons
-	var sq = $jit.id("r-sq"),
-		st = $jit.id("r-st"),
-		sd = $jit.id("r-sd");
-	var util = $jit.util;
-	util.addEvent(sq, "change", function () {
-		if (!sq.checked) return;
-		util.extend(tm, new $jit.Layouts.TM.Squarified());
-		tm.refresh();
-	});
-	util.addEvent(st, "change", function () {
-		if (!st.checked) return;
-		util.extend(tm, new $jit.Layouts.TM.Strip());
-		tm.layout.orientation = "v";
-		tm.refresh();
-	});
-	util.addEvent(sd, "change", function () {
-		if (!sd.checked) return;
-		util.extend(tm, new $jit.Layouts.TM.SliceAndDice());
-		tm.layout.orientation = "v";
-		tm.refresh();
-	});
-	//add event to the back button
-	var back = $jit.id("back");
-	$jit.util.addEvent(back, "click", function () {
-		tm.out();
 	});
 }
 
