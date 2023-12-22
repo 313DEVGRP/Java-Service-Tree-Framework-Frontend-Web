dff1566fe129df54642a59433f1a91c97bca2a09
hsYang <gudtjr4662@gmail.com>
2023-12-23 02:22:55 +0900

    feat: [ARMS-189] #comment 스코프 분석 나이팅게일 > RadialPolarBarChart 변경 - 23.12.22 #close #time 1h +review SR @HSYang-Hub
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    요구 관리 : [Requirement Manage] http://www.a-rms.net
    문서 관리 : [Document] http://www.313.co.kr/confluence
    이슈 관리 : [IssueTracker] http://www.313.co.kr/jira
    소스 관리 : [VersionControl] https://github.com/313devgrp
    코드 리뷰 : [CodeReview] http://www.313.co.kr/fecru
    빌드 관리 : [CICD-Deploy] http://www.313.co.kr/jenkins
    배포 관리 : [Deploy-Pipeline] http://www.313.co.kr/spinnaker
    아티 팩트 : [ArtifactManager] http://www.313.co.kr/nexus
    코드 분석 : [CodeAnalysis] http://www.313.co.kr/sonar
    성능 측정 : [ZipKin] http://www.313.co.kr/zipkin/
    
    도커 관리 : [Docker Hub] https://hub.docker.com/u/313devgrp
    클러스터1 : [Kubernetes] http://www.313.co.kr:31380
    클러스터2 : [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    디비 관리 : [DB Admin] http://www.313.co.kr/phpmyadmin/
    오브 젝트 : [S3 Admin] http://www.313.co.kr/minio/login
    캐시 관리 : [Cache Admin] http://www.313.co.kr/redis/
    ELFK 스택1 : [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    ELFK 스택2 : [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    ELFK 스택3 : [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    ELFK 스택4 : [Kibana] http://www.313.co.kr/kibana/app/home#/
    
    모니터링 1 : [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    모니터링 2 : [Grafana] http://www.313.co.kr/grafana
    
    인증 인가 : [Auth] http://www.313.co.kr/auth/
    
    NFS 관리 : [NAS] http://www.313.co.kr:5000/webman/index.cgi
    메일 관리 : [Mail] http://www.313.co.kr/mail/
    원격 관리 : [RDP] http://www.313.co.kr/guacamole/#/

--- arms/js/analysis/resource/chart/RadialPolarBarChart.js
+++ arms/js/analysis/resource/chart/RadialPolarBarChart.js
@@ -72,22 +72,64 @@ function drawRadialPolarBarChart(target, dataArr, colorArr) {
     var myChart = echarts.init(chartDom);
     var option;
 
+    var defaultColorSet = [
+        "rgba(227,26,27,0.66)",
+        "rgba(55,125,184,0.62)",
+        "rgba(77,175,74,0.65)",
+        "rgba(255,127,0,0.7)",
+        "rgba(255,255,51,0.71)",
+        "rgba(151,78,163,0.73)",
+        "rgba(166,86,40,0.7)"
+    ];
+
+    function calculateTotal(dataArr) {
+        const total = dataArr.reduce((acc, curr) => acc + curr.value, 0);
+        return total;
+    }
+    const totalValue = calculateTotal(dataArr);
+    console.log ("totalValue => " + totalValue);
+    var angleAxisArr = [], seriesDataArr=[];
+    if(dataArr.length > 0) {
+        dataArr.forEach((element, idx) => {
+           angleAxisArr.push(element["name"]);
+           seriesDataArr.push({value: element["value"], itemStyle: { color: colorArr ? colorArr[idx] : defaultColorSet[idx] }});
+        });
+    }
+
     option = {
-        title: [
-            {
-                text: 'Radial Polar Bar Label Position (middle)'
-            }
-        ],
         polar: {
-            radius: [30, '80%']
+            radius: ["25%", '55%'],
+            center: ['50%', '50%']
         },
         radiusAxis: {
-            max: 4
+            max: (totalValue === 0 ? 5 : totalValue),
+            splitNumber: 5,
+            splitLine: {
+                show: true,
+                lineStyle: {
+                    color: "gray" ,
+                    width: 0.5,
+                    type: "dashed"
+                }
+            },
+            axisLabel: {
+                textStyle: {
+                    color: 'white', // 원하는 색상으로 변경
+                    fontSize: 10, // 폰트 크기 조정
+                }
+            }
         },
         angleAxis: {
             type: 'category',
-            data: ['a', 'b', 'c', 'd'],
-            startAngle: 90
+            data: angleAxisArr,//['BaseVersion', '1.0.1', '1.0.0'],
+            startAngle: 90,
+            axisLabel: {
+                textStyle: {
+                    color: 'white', // 원하는 색상으로 변경
+                    fontSize: 12, // 폰트 크기 조정
+                    fontWeight: "bold"
+                }
+            }
         },
         graphic: [
             {
@@ -97,16 +139,16 @@ function drawRadialPolarBarChart(target, dataArr, colorArr) {
                 z: 100,
                 style: {
                     text: [
-                        'Total',
-                        '100' // 여기에 값들의 총 합을 계산하여 넣어주세요.
+                        '{a|Total}',
+                        '{a|  ' + totalValue + '}'
                     ].join('\n'),
                     rich: {
                         a: {
-                            fontSize: 15,
+                            fontSize: 14,
                             fontWeight: 'bold',
-                            lineHeight: 30,
+                            lineHeight: 20,
                             fontFamily: 'Arial',
-                            fill: 'red'
+                            fill: 'white'
                         }
                     }
                 }
@@ -114,24 +156,79 @@ function drawRadialPolarBarChart(target, dataArr, colorArr) {
         ],
         series: {
             type: 'bar',
-            data: [
-                { value: 2, itemStyle: { color: '#ff0000' } }, // 각 angle에 맞는 색상으로 수정
-                { value: 1.2, itemStyle: { color: '#00ff00' } },
-                { value: 2.4, itemStyle: { color: '#0000ff' } },
-                { value: 3.6, itemStyle: { color: '#ffff00' } }
-            ],
+            data: seriesDataArr,
             coordinateSystem: 'polar',
-            label: {
-                show: true,
-                position: 'middle',
-                formatter: '{b}: {c}'
+            // label: {
+            //     show: true,
+            //     position: 'inside',
+            //     formatter: function (params) {
+            //         if(params.value <= 10) {
+            //             return params.name + '\n' + '(' +params.value+')11';
+            //         } else {
+            //             return params.name + '\n' + '(' +params.value+')';
+            //         }
+            //     }
+            // },
+            barWidth: "100%",
+            itemStyle: {
+                borderColor: "white",
+                borderWidth: 1,
+                barBorderRadius: 3
             }
         },
         animation: false
-    };
+    }
 
-    option && myChart.setOption(option);
-    // 리사이즈 기능
+    function replaceNaN(value) {
+        if (isNaN(value)) {
+            return " - ";
+        } else {
+            return value;
+        }
+    }
+
+    function drawChartWithFooter(dataArr,total) {
+        const existingChartFooter = document.querySelector('.chart-footer');
+
+        if (existingChartFooter) {
+            existingChartFooter.remove();
+        }
+
+        const chartFooter = document.createElement("div");
+        chartFooter.classList.add("chart-footer");
+
+        dataArr.forEach((data,index) => {
+            const item = document.createElement("div");
+            const portion =replaceNaN(+(data.value*100/ +total).toFixed(0));
+            item.classList.add("footer-item");
+            item.style.borderColor = colorArr[index];
+            item.innerHTML = `<div class="item-name">${data.name}</div> <div class="item-value">${data.value} (${portion}%)</div>`;
+            chartFooter.appendChild(item);
+        });
+
+        chartDom.appendChild(chartFooter);
+
+        const footerItems = document.querySelectorAll('.footer-item');
+        const itemCount = footerItems.length;
+
+        const remainder = itemCount % 3;
+        const quotient = Math.floor(itemCount / 3);
+
+        footerItems.forEach((item, index) => {
+            if (remainder === 1 && Math.floor(index / 3) === quotient) {
+                item.style.width = '100%';
+            } else if (remainder === 2 && Math.floor(index / 3) >= quotient) {
+                item.style.width = '50%';
+            } else {
+                item.style.width = '33.33%';
+            }
+        });
+    }
+
+    drawChartWithFooter(dataArr,totalValue);
+
+    option && myChart.setOption(option, true);
+
     window.addEventListener('resize', function () {
         myChart.resize();
     });
