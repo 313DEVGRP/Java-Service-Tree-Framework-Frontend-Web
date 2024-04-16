ed66037b854bbbf4f60d29c26e4fcd4f227d6a31
hsYang <gudtjr4662@naver.com>
2024-04-16 20:06:33 +0900

    feat: [YHS-73] #comment 톱메뉴 - 일정 시작전 표시 방식 수정, 간트 - 미사용 함수 삭제 - 24.04.16 #close #time 1h +review SR @HSYang-hub
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF):: hsYang-Hub ]
    요구 관리 : [Requirement Manage] http://www.a-rms.net
    문서 관리 : [Document] http://www.313.co.kr/confluence
    이슈 관리 : [IssueTracker] http://www.313.co.kr/jira
    소스 관리 : [VersionControl] https://github.com/313devgrp
    코드 리뷰 : [CodeReview] http://www.313.co.kr/fecru
    빌드 관리 : [CICD-Deploy] http://www.313.co.kr/jenkins
    배포 관리 : [Deploy-Pipeline] http://www.313.co.kr/spinnaker
    아티 팩트 : [ArtifactManager] http://www.313.co.kr/nexus
    코드 분석 : [CodeAnalysis] http://www.313.co.kr/sonar
    성능 측정 : [ZipKin] http://www.313.co.kr/zipkin/
    
    도커 관리 : [Docker Hub] https://hub.docker.com/u/313devgrp
    클러스터1 : [Kubernetes] http://www.313.co.kr:31380
    클러스터2 : [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    디비 관리 : [DB Admin] http://www.313.co.kr/phpmyadmin/
    오브 젝트 : [S3 Admin] http://www.313.co.kr/minio/login
    캐시 관리 : [Cache Admin] http://www.313.co.kr/redis/
    ELFK 스택1 : [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    ELFK 스택2 : [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    ELFK 스택3 : [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    ELFK 스택4 : [Kibana] http://www.313.co.kr/kibana/app/home#/
    
    모니터링 1 : [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    모니터링 2 : [Grafana] http://www.313.co.kr/grafana
    
    인증 인가 : [Auth] http://www.313.co.kr/auth/
    
    NFS 관리 : [NAS] http://www.313.co.kr:5000/webman/index.cgi
    메일 관리 : [Mail] http://www.313.co.kr/mail/
    원격 관리 : [RDP] http://www.313.co.kr/guacamole/#/

--- arms/js/reqGantt.js
+++ arms/js/reqGantt.js
@@ -314,8 +314,6 @@ function makeVersionMultiSelectBox() {
 			TopMenuApi.톱메뉴_초기화();
 			TopMenuApi.톱메뉴_세팅();
 
-			//통계로드
-			statisticsLoad($("#selected_pdService").val(), selectedVersionId);
 			//진행상태 가져오기
 			progressLoad($("#selected_pdService").val(), selectedVersionId);
 		}
@@ -354,8 +352,7 @@ function bind_VersionData_By_PdService() {
 				// 최상단 메뉴 세팅
 				TopMenuApi.톱메뉴_초기화();
 				TopMenuApi.톱메뉴_세팅();
-				//통계로드
-				statisticsLoad($("#selected_pdService").val(), selectedVersionId);
+
 				//진행상태 가져오기
 				progressLoad($("#selected_pdService").val(), selectedVersionId);
 
