a0c9382bf7b55b218fb092730043c323d803d758
dumbbelloper <dumbbelloper@gmail.com>
2024-06-16 21:21:51 +0900

    feat: [DEMO-288] #comment swiper.js 썸네일 갤러리 추가 - 24.06.16
    #close #time 1h +review SR @313devops
    
    - css width, height 관련 이슈(명확한 원인 파악 X) 반응형과 사이즈 크기 조절은 추후 적용
    
    추신 : 맨앞에 붙일 수 있는 구분
    •fix : Bug 류 처리 시 ( patch version up )
    •feat : 신규 기능 류 처리 시 ( minor version up )
    •perf : 메이저 기능 류 처리 시 ( major version up )
    
    코드리뷰는 코드를 통해 소통하고, 서로의 경험을 통해 배우면서 성장 할 수 있는 기회입니다.
    나를 위해, 나의 동료를 위해, 우리 팀을 위해 리뷰 시간을 사용해 주세요.
    •너무 많은 코드를 한꺼번에 리뷰하지 마세요.
    •최소한의 설명과 함께 중점적으로 봐야 할 곳을 알려 주세요.
    •리뷰 후 Comment를 남겨 주세요.
    •리뷰는 빠른 시간에 응답해 주세요.
    •지적 보다는 대안을 제시해 주세요.
    
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/
    [DrawIO] http://www.313.co.kr/drawio

--- arms/js/pdService.js
+++ arms/js/pdService.js
@@ -753,47 +753,69 @@ function detailClick(element, c_id) {
 }
 
 function addImageToSwiper(imageSrcArray, swiperContainerId) {
-	var swiperInstance = document.querySelector('#' + swiperContainerId).swiper;
+	var mainSwiperContainer = document.querySelector('#' + swiperContainerId + ' .swiper-wrapper');
+	var thumbsSwiperContainer = document.querySelector('#' + swiperContainerId + '_thumbs' + ' .swiper-wrapper');
 
-	if (!swiperInstance) {
-		swiperInstance = new Swiper('#' + swiperContainerId, {
-			pagination: {
-				el: '.swiper-pagination',
-				// clickable: true,
-				type: "fraction",
-			},
-				slidesPerView: 1,
-				spaceBetween: 0,
-			navigation: {
-				nextEl: '.swiper-button-next',
-				prevEl: '.swiper-button-prev',
-			},
-		});
-	} else {
+	var swiperInstance = document.querySelector('#' + swiperContainerId).swiper;
+	var thumbsSwiperInstance = document.querySelector('#' + swiperContainerId + '_thumbs').swiper;
+
+	if (swiperInstance) {
 		swiperInstance.removeAllSlides();
 	}
 
-	for (var i = 0; i < imageSrcArray.length; i++) {
-		var newSlideElement = document.createElement('div');
-
-		newSlideElement.className = 'swiper-slide';
-
-		var newImageElement = document.createElement('img');
-
-		newImageElement.src = imageSrcArray[i];
-
-		newSlideElement.appendChild(newImageElement);
-
-		swiperInstance.appendSlide(newSlideElement);
+	if (thumbsSwiperInstance) {
+		thumbsSwiperInstance.removeAllSlides();
 	}
 
-	var slides = document.querySelectorAll('#' + swiperContainerId + ' .swiper-slide');
+	for (var i = 0; i < imageSrcArray.length; i++) {
+		var mainSlide = document.createElement('div');
+		mainSlide.className = 'swiper-slide';
+		var mainImage = document.createElement('img');
+		mainImage.src = imageSrcArray[i];
+		mainSlide.appendChild(mainImage);
+		mainSwiperContainer.appendChild(mainSlide);
+
+		var thumbsSlide = document.createElement('div');
+		thumbsSlide.className = 'swiper-slide';
+		var thumbsImage = document.createElement('img');
+		thumbsImage.src = imageSrcArray[i];
+		thumbsSlide.appendChild(thumbsImage);
+		thumbsSwiperContainer.appendChild(thumbsSlide);
+	}
+
+	thumbsSwiperInstance = new Swiper('#' + swiperContainerId + '_thumbs', {
+		spaceBetween: 0,
+		// slidesPerView: imageSrcArray.length,
+		slidesPerView: 4,
+		freeMode: true,
+		watchSlidesVisibility: true,
+		watchSlidesProgress: true,
+	});
+
+	swiperInstance = new Swiper('#' + swiperContainerId, {
+		spaceBetween: 10,
+		pagination: {
+			el: '.swiper-pagination',
+			// clickable: true,
+			type: "fraction",
+		},
+		slidesPerView: 1,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		thumbs: {
+			swiper: thumbsSwiperInstance,
+		},
+	});
+
+	var slides = document.querySelectorAll("#" + swiperContainerId + " .swiper-slide");
 	if (slides.length === 1) {
-		document.querySelector('#' + swiperContainerId + ' .swiper-pagination').style.display = 'block';
-		document.querySelector('#' + swiperContainerId + ' .swiper-button-next').style.display = 'block';
-		document.querySelector('#' + swiperContainerId + ' .swiper-button-prev').style.display = 'block';
-		swiperInstance.navigation.nextEl.classList.add('swiper-button-disabled');
-		swiperInstance.navigation.prevEl.classList.add('swiper-button-disabled');
+		document.querySelector("#" + swiperContainerId + " .swiper-pagination").style.display = "block";
+		document.querySelector("#" + swiperContainerId + " .swiper-button-next").style.display = "block";
+		document.querySelector("#" + swiperContainerId + " .swiper-button-prev").style.display = "block";
+		swiperInstance.navigation.nextEl.classList.add("swiper-button-disabled");
+		swiperInstance.navigation.prevEl.classList.add("swiper-button-disabled");
 	}
 }
 
