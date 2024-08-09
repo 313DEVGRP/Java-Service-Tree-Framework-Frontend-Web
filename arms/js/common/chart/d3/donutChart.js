d8931e4514866dce35209fc86e9b6be8434252ff
hsYang <gudtjr4662@naver.com>
2024-08-09 23:10:49 +0900

    feat: [YHS-275] #comment Full Data Sheet 중간커밋 - 24.08.09 #close #time 1h +review SR @HSYang-hub
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF):: hsYang-Hub ]
    요구 관리 : [Requirement Manage] http://www.a-rms.net
    문서 관리 : [Document] http://www.313.co.kr/confluence
    이슈 관리 : [IssueTracker] http://www.313.co.kr/jira
    소스 관리 : [VersionControl] https://github.com/313devgrp
    코드 리뷰 : [CodeReview] http://www.313.co.kr/fecru
    빌드 관리 : [CICD-Deploy] http://www.313.co.kr/jenkins
    배포 관리 : [Deploy-Pipeline] http://www.313.co.kr/spinnaker
    아티 팩트 : [ArtifactManager] http://www.313.co.kr/nexus
    코드 분석 : [CodeAnalysis] http://www.313.co.kr/sonar
    성능 측정 : [ZipKin] http://www.313.co.kr/zipkin/
    
    도커 관리 : [Docker Hub] https://hub.docker.com/u/313devgrp
    클러스터1 : [Kubernetes] http://www.313.co.kr:31380
    클러스터2 : [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    디비 관리 : [DB Admin] http://www.313.co.kr/phpmyadmin/
    오브 젝트 : [S3 Admin] http://www.313.co.kr/minio/login
    캐시 관리 : [Cache Admin] http://www.313.co.kr/redis/
    ELFK 스택1 : [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    ELFK 스택2 : [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    ELFK 스택3 : [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    ELFK 스택4 : [Kibana] http://www.313.co.kr/kibana/app/home#/
    
    모니터링 1 : [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    모니터링 2 : [Grafana] http://www.313.co.kr/grafana
    
    인증 인가 : [Auth] http://www.313.co.kr/auth/
    
    NFS 관리 : [NAS] http://www.313.co.kr:5000/webman/index.cgi
    메일 관리 : [Mail] http://www.313.co.kr/mail/
    원격 관리 : [RDP] http://www.313.co.kr/guacamole/#/

--- arms/js/common/chart/d3/donutChart.js
+++ arms/js/common/chart/d3/donutChart.js
@@ -106,3 +106,98 @@ function donutChart(pdServiceLink, pdServiceVersionLinks) {
 		}
 	});
 }
+
+/*
+* fullDataSheet - 사용. 페이지를 위한 별도 API 분리 가능성 검토.
+*/
+function drawDonutChart_report(targetElement, data, cardWidth) {
+	let $targetElementId =  "#" + targetElement;
+
+	let stateData = data;
+	let stateKey = ["open", "in-progress", "resolved", "closed"];
+
+	function donutChartNoData() {
+		c3.generate({
+			bindto: $targetElementId, // targetElement
+			data: {
+				columns: [],
+				type: 'donut',
+			},
+			donut: {
+				title: "Total : 0"
+			},
+		});
+	}
+
+	if(stateData === undefined || stateData === null || Object.keys(stateData).length === 0) {
+		donutChartNoData();
+		return false;
+	}
+
+	let totalDocCount = stateData.total; // data.전체합계;
+	const columnsData = [];
+	// data.검색결과['group_by_status.status_name.keyword'];
+
+	stateKey.forEach( key => {
+		if(stateData.hasOwnProperty(key)) {
+			columnsData.push([key,stateData[key]]);
+		}
+	});
+
+	let size = {
+		width: cardWidth * 0.7,   // 가로 크기
+		height: cardWidth * 0.7   // 세로 크기
+	}
+	console.log(size);
+	const chart = c3.generate({
+		bindto: $targetElementId,
+		data: {
+			columns: columnsData,
+			type: 'donut'
+		},
+		donut: {
+			title: "Total : " + totalDocCount,
+			width: cardWidth * 0.06
+		},
+		size: size,
+		color: {
+			pattern: ColorPalette.common.reqStateColor
+		},
+		tooltip: {
+			format: {
+				value: function (value, ratio, id, index) {
+					return value;
+				}
+			},
+		},
+	});
+
+	$(document).on('click', $targetElementId + ' .c3-legend-item', function () {
+		const id = $(this).text();
+		let isHidden = false;
+
+		if($(this).hasClass('c3-legend-item-hidden')) {
+			isHidden = false;
+			$(this).removeClass('c3-legend-item-hidden');
+		} else {
+			isHidden = true;
+			$(this).addClass('c3-legend-item-hidden');
+		}
+		let docCount = 0;
+
+		for (const key of stateKey) {
+			if (key === id) {
+				docCount = stateData[key];
+				break;
+			}
+		}
+		if (docCount) {
+			if (isHidden) {
+				totalDocCount -= docCount;
+			} else {
+				totalDocCount += docCount;
+			}
+		}
+		$($targetElementId+' .c3-chart-arcs-title').text("Total : " + totalDocCount);
+	});
+}
